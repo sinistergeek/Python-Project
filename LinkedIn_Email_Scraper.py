a39ceec9a9dd6709c107b71478813d474a75257e
sinistergeek <susanbhattraidon@gmail.com>
2023-11-30 21:59:52 +0545

    LinkedIn_Email_Scraper

--- LinkedIn_Email_Scraper.py
+++ LinkedIn_Email_Scraper.py
@@ -9,4 +9,6 @@ def LinkedInEmailScraper(userEmail,userPassword):
     url = '[INSERT URL TO LINKEDIN POST]'
     browser.get(url)
     browser.implicity_wait(5)
-
+    commentDiv = browser.find_element_by_xpath('/html/body/main/section[1]/section[1]/div/div[3]/a[2]')
+    loginLink = commentDiv.get_attriute('href')
+    browser.get(loginLink)
