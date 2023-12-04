17dccd7d4e78b0b7ba0c2e26afbe12189d515f95
sinistergeek <susanbhattraidon@gmail.com>
2023-12-04 09:26:52 +0545

    Movie_Info_Telegram_Bot

--- Movie_Info_Telegram_Bot.py
+++ Movie_Info_Telegram_Bot.py
@@ -106,6 +106,26 @@ def get_info(movie):
                 lis.append(details)
                 pre_url = new_url
                 count += 1
+                res.append(lis)
+    except:
+        pass
+return res
 
+def main():
+    updater = Updater(TOKEN,use_context = True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",help))
+    dp.add_handler(CommandHandler("name",name))
+    dp.add_handler(CommandHandler("genre",genre))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+
+if __name__=='__main__':
+    main()
 
 
