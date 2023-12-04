af915ffa2a6a2884bce29a8672d13013bf4f6c9c
sinistergeek <susanbhattraidon@gmail.com>
2023-12-04 08:14:55 +0545

    Data_Cleaning_Source_code

--- Data_Cleaning_Source_code.py
+++ Data_Cleaning_Source_code.py
@@ -17,5 +17,29 @@ msg.rename(columns={"v1":"label","v2":"text"},inplace=True)
 
 for i in msg.index:
     if msg['label'][i] =='ham':
+        msg['label'][i] = 0
+    else:
+        msg['label'][i] = 1
+msg = msg.drop_duplicates()
 
+msg['cleaned_text'] = ""
+for i in msg.index:
+    updated_list=[]
+for j in range(len(msg['text'][i])):
+    if msg['text'][i][j] notin string.punctuation:
+        if msg['text'][i][j].isdigit() == False:
+            updated_list.apppend(msg['text'][i][j])
+msg['cleaned_text'][i] = updated_string
+msg.drop(['text'],axis=1,inplace=True)
+
+msg['token'] = ""
+for i in msg.index:
+    msg['token'][i] = re.split("\W+",msg['cleaned_text'][i].lower())
+wordlem = nltk.WordNetLemmatizer()
+for i in msg.index:
+    updated_list = []
+    for j in range(len(msg['updated_token'][i])):
+        updated_list.append(wordlem.lemmatize(msg['ipdated_token'][i][j]))
+    msg['lem_text'][i] = updated_list
+msg.drop(['updated_token'],axis=1,inplace=True)
     
