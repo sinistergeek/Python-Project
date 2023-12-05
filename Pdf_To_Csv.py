a92bd6ce0b73001ec3d956fcb2cb3021860bea08
sinistergeek <susanbhattraidon@gmail.com>
2023-12-05 21:00:17 +0545

    Pdf_To_Csv

--- Pdf_To_Csv.py
+++ Pdf_To_Csv.py
@@ -29,4 +29,40 @@ def pdf_csv():
         pdf_flag = True
     else:
         print("[-+-] looking for another pdf...")
+    arr_pdf = [
+            defaultdir for defaultdir in os.listdir()
+            if defaultdir.endswith(".pdf")
+               ]
+    if len(arr_pdf) ==1:
+        print("[-+-] pdf found:" + arr_pdf[0] + "\n")
+        pdf_path = os.path.join(defaultdir,arr_pdf[0])
+        pdf_flag = True
+    elif len(arr_pdf) > 1:
+        print("[-+-] more than 1 pdf found,exiting script!")
+        pdf_flag = False
+    else:
+        print("[-+-] pdf cannt be found, exiting script!")
+        pdf_flag = False
 
+if pdf_flag == True:
+    try:
+        print("[-+-] looking for default csv...")
+        open(csv_path,"r")
+        print("[-+-] csv found:" + csv + "\n")
+
+    except IOError:
+        print("[-+-] did not find csv at default file path!")
+        print("[-+-] creating a blank csv file:" + csv + "...\n")
+        open(csv_path,"w")
+
+    print("[-+-] converting pdf to csv...")
+
+    try:
+        tabula.convert_int(pdf_path,csv_path,output_format="csv",pages="all")
+        print("[-+-] pdf to csv conversion complete!\n")
+    except IOError:
+        print("[-+-] pdf to csv conversion failed")
+
+    print("[-+-] converted csv file can be found here. "+csv_path+"\n")
+    print("[-+-] finished pdf_csv.py successfully!")
+pdf_csv()
