d8901720b298feac14de9175442b05dbf3793e9d
sinistergeek <susanbhattraidon@gmail.com>
2023-12-06 09:36:20 +0545

    Pomodoro_Clock_GUI

--- Pomodoro_Clock_GUI.py
+++ Pomodoro_Clock_GUI.py
@@ -44,4 +44,38 @@ def block_websites():
         tk.messagebox.showinfo("Error","Functionally not supported in your OS!")
         web_var.set("")
 
+def remove_websites():
+    global block_list
+    global host_path
+    try:
+        if enable:
+            with open(host_path,"r+") as file:
+                content = file.readlines()
+                file.seek(0)
+                for lines in content:
+                    if not any(website in lines for website in block_list):
+                        file.write(lines)
+                file.truncate()
+            block_list.clear()
+            enable=0
+    except:
+        pass
+    finally:
+        pass
+def blocker():
+
+    global enable
+    global popup_4
+    popup_4 = tk.Topleve(root)
+    popup_4.title("Website Blocker!")
+    popup_4.geometry("360x220")
+    popup_4.config(bg='DodgerBlue4')
+
+    global block_list
+    global web_var
+    web_var = tk.StringVar()
+
+    pass_label = tk.Label(popup_4,text='Enter URL to block:',font=('Arial',12,'bold'),bg='DodgerBlue4',fg='white')
+    pass_entry = tk.Entry(popup_4,textvariable=web_var,font=('Arial',12,'bold'))
+    sub_btn = tk.Button(popup_4,text = 'Block',font =('Arial',12,'bold'),command= block_websites,bg='gold',activebackground='yellow')
 
