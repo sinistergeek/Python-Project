62cbae98aefb10403fba17b25876302ab03abe73
sinistergeek <susanbhattraidon@gmail.com>
2023-12-06 21:12:27 +0545

    Pyweather

--- Pyweather.py
+++ Pyweather.py
@@ -21,3 +21,6 @@ if x['cod'] !='404':
     wind_direction = q['deg']
     k = x['coulds']
     cloudliness = k['all']
+    print('Temperature(inkelvin)='+str(current_temperature)+'\n Atmospheric Pressure(in hPa)='+str(current_pressure)+'\n Humidity(in percentage)='+str(current_humidity)+'\n wind speed(in m/s)='+str(wind_speed)+'\n Wind Direction(in degress)='+str(wind_direction)+'\n Cloudliness(in percentage) ='+str(cloudliness)+'\n Weather Description='+str(weather_description))
+else:
+    print('city Not Found')
