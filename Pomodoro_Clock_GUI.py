c20a6e65ad94c0584676c13cf6e75bfe298b2e25
sinistergeek <susanbhattraidon@gmail.com>
2023-12-06 09:22:04 +0545

    Pomodoro_Clock_GUI

---
+++ Pomodoro_Clock_GUI.py
@@ -0,0 +1,18 @@
+import time
+import tkinter as tk
+from tkinter import messagebox
+import pygame
+from datetime import timedelta
+
+
+pygame.mixer.init()
+pomo_count = 0
+break_count = 0
+enable = 0
+
+host_path = r"~/Downloads"
+
+block_list = []
+
+redirect = "127.0.0.1"
+
