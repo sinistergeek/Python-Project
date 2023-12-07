112020e6820c0eb3ccf30e9bee76c3c68d760ccb
sinistergeek <susanbhattraidon@gmail.com>
2023-12-07 16:47:01 +0545

    Quote_Scraper

--- Quote_Scraper.py
+++ Quote_Scraper.py
@@ -14,4 +14,27 @@ try:
     dictwriter = csv.DictWriter(csv_file,fieldnames=fieldnames)
 
     dictwriter.writeheader()
+    while True:
+        for quote in bs.findAll('div',{'class':'quote'}):
+            text=quote.find('span',{'class':'text'}).text
+            author=quote.find('small',{'class':'author'}).text
+            tags = []
+            for tag in quote.findAll('a',{'class':'tag'}):
+                tags.append(tag.text)
+
+            dictwriter.writerow({'quote':text,'author':author,'tags':tags})
+
+        next = bs.find('li',{'class':'next'})
+        if not next:
+            break
+
+        html = requests.get(url+next.a.attrs['herf'])
+        bs = BeautifulSoup(html.text,'html.parser')
+
+except:
+    print('Unknown Error!!')
+
+finally:
+    csv_file.close()
+
 
