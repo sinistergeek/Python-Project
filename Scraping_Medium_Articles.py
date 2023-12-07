53a125de78203f90ccb6461c3738dac49e5d7f9a
sinistergeek <susanbhattraidon@gmail.com>
2023-12-07 17:13:15 +0545

    Scraping_Medium_Articles

--- Scraping_Medium_Articles.py
+++ Scraping_Medium_Articles.py
@@ -17,3 +17,36 @@ def get_page():
         res.raise_for_status()
         soup = BeautifulSoup(res.text,'html.parser')
         return soup
+
+def puriy(text):
+    rep = {"<br>":"\n","<br/>":"\n","<li>":"\n"}
+    rep = dict((re.escape(k),v) for k,v in rep.items())
+    pattern = re.compile("|".join(rep.keys()))
+    text = pattern.sub(lambda m: rep[re.escape(m.group(0))],text)
+    text = re.sub('\<(.*?)\>','',text)
+    return text
+
+def collect_text(soup):
+    fin = f'url:{url}\n\n'
+    main = (soup.head.title.text).split('|')
+    global title
+    title = main[0].strip()
+    fin += f'Title:{title.upper()}\n{main[1].strip()}'
+    header = soup.find_all('h1')
+    j=1
+    try:
+        fin +='\n\nINTRODUCTION\n'
+        for elem in list(header[j].previous_siblings)[::-1]:
+            fin += f'\n{purify(str(elem))}'
+    except:
+        pass
+    fin +=f'\n\n{header[j].text.upper()}'
+    for elem in header[j].next_siblings:
+        if elem.name == 'h1':
+            j+=1
+            fin += f'\n\n{header[j].text.upper()}'
+            continue
+        fin += f'\n{purify(str(elem))}'
+        return fin
+
+
