1f49a6c40a917352f6920173a7849d17b33dd219
sinistergeek <susanbhattraidon@gmail.com>
2023-12-07 11:38:17 +0545

    Room_security

--- Room_security.py
+++ Room_security.py
@@ -13,4 +13,11 @@ def gen(camera):
         yield(b'--frame\r\n'b'Context-Type:image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
+@app.route('/video_feed')
+
+def video_feed():
+    return Response(gen(VideoCamera()),mimetype='multipart/x-mixed-replace;boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='5000',debug=False)
 
