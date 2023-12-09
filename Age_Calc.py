3d063e11cfd696f67a85c4c1edc6951b72aa7661
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 08:31:54 +0545

    Age_Calc

--- Age_Calc.py
+++ Age_Calc.py
@@ -17,3 +17,26 @@ def ageCalc():
         age -= 1
         statement = Label(text=f'{nameValue.get()}s age is{age}.')
         statement.gird(row=6,column=1,pady=15)
+
+l1 = Label(text="Name:")
+l1.grid(row=1,column=0)
+nameValue = StringVar()
+
+nameEntry = Entry(root,textvariable=nameValue)
+nameEntry.grid(row=1,column=1,padx=10,pady=10)
+
+
+l2 = Label(text="Year: ")
+l2.grid(row=2,column=0)
+yearValue = StringVar()
+yearEntry = Entry(root,textvariable = yearValue)
+yearEntry.grid(row=2,column=1,padx=10,pady=10)
+
+l3 = Label(text="Month: ")
+l3.grid(row=3,column=0)
+monthValue = StringVar()
+monthEntry = Entry(root,textvariable=monthValue)
+monthEntry.grid(row=3,column=1,padx=10,pady=10)
+
+l4 = Label(text="Day: ")
+l4.grid(row=4,column=0)
