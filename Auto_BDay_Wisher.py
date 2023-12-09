96adb1d354434e2e13848634d55fa0b0f9458767
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 14:35:54 +0545

    Auto_BDay_Wisher

--- Auto_BDay_Wisher.py
+++ Auto_BDay_Wisher.py
@@ -9,4 +9,10 @@ os.chdir(current_path)
 GMAIL_ID = input("Enter your email:")
 GMAIL_PSWD = input("Enter Password for your email mentioned above:")
 
-
+def sendEmail(to,sub,msg):
+    print(f'Email to {to} send: \n Subject:{sub},\nMessage:{msg}')
+    s = smtplib.SMTP('smtp.gmail.com',587)
+    s.starttls()
+    s.login(GMAIL_ID,GMAIL_PSWD)
+    s.sendmail(GMAIL_ID,to,f"Subject: {sub} \n\n {msg}")
+    s.quit()
