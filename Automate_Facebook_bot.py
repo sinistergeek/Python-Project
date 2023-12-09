847e7ce3af057f8b5ba1972df3e6dd630eff7ae2
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 18:37:06 +0545

    Automate_Facebook_bot

--- Automate_Facebook_bot.py
+++ Automate_Facebook_bot.py
@@ -14,3 +14,25 @@ groupid = []
 for i in lists:
     groupid.append(i)
     message = input("Enter your message:")
+
+
+class FacebookLogin():
+    def __init__(self,email,password,browser='Chrome'):
+        self.email = email
+        self.password = password
+        if browser == 'Chrome':
+            self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+            seelf.driver.get(LOGIN_URL)
+            time.sleep(1)
+
+    def login(self):
+        email_element = self.driver.find_element_by_id('email')
+        email_element.send_keys(self.email)
+        password_element = self.driver.find_element_by_id('pass')
+        password_element.send_keys(self.password)
+        login_button = self.driver.find_element_by_id('loginbutton')
+        login_button.click()
+
+        time.sleep(2)
+
+
