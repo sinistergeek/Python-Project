08bada2cbd04438833ba25a186de4c2b792faa7e
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 17:04:32 +0545

    Automatic_Backup

--- Automatic_Backup.py
+++ Automatic_Backup.py
@@ -9,4 +9,11 @@ import threading
 def parse_input():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t','--target',nargs=1,required=True,help='Target Backup folder')
+    parser.add_argument('-s','--source',nargs='+',required=True,help='Source File to be added')
+    parser.add_argument('-c','--compress',nargs=1,type=int,help='Gzip threshold in bytes,Deafult 1024KB',default=[102400])
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+    return parser.parser_args()
+
 
