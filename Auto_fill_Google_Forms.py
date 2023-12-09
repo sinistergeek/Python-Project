b7a56f5be9dcc12033acb198ddb462c95649a538
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 18:11:10 +0545

    Auto_fill_Google_Forms

---
+++ Auto_fill_Google_Forms.py
@@ -0,0 +1,10 @@
+import csv
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+inputName = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div/div[1]/div/div[1]/input'
+inputEmailID = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
+inputPhone = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
+
+Submit = '/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div/div'
