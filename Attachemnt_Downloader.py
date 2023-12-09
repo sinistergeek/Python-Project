93c100c2f0a0051ed8bfdd8dc3498d1c35284ce9
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 14:21:13 +0545

    Attachemnt_Downloader

--- Attachemnt_Downloader.py
+++ Attachemnt_Downloader.py
@@ -15,3 +15,24 @@ def attachmentdownload(resultthreads):
     except:
         raise Exception("Error occured while downloading attachment(s).")
 
+if __name__ == '__main__':
+    query = input("Enter search query: ")
+    newquery = query + "+ has:attachment"
+    resulthreads = ezgmail.search(newquery)
+    if len(resulthreads) == 0:
+        print("Result has no attachments:")
+    else:
+        print("Result(s) with attachments:")
+        for threads in resulthreads:
+            print(f"Email Subject: {threads.messages[0].subject}")
+        try:
+            ask = input("Do you want to download attachments(s) in result(s) (Yes/No)?")
+            if ask == "Yes":
+                attachmentdownload(resulthreads)
+            else:
+                print("Program exited")
+
+        except:
+            print("Something went wrong")
+
+
