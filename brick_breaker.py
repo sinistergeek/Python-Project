952f73c69129dd68d840c2fee93023902aa79b30
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 21:34:03 +0545

    brick_breaker

--- brick_breaker.py
+++ brick_breaker.py
@@ -12,3 +12,44 @@ O_brick = (255,100,10)
 w_brick = (255,255,255)
 g_brick = (0,255,0)
 black = (0,0,0)
+game_rows = 6
+game_coloumns = 6
+clock = pygame.time.Clock()
+frame_rate = 60
+my_ball = False
+game_over = 0
+score = 0
+
+class Ball():
+    def __init__(self,x,y):
+        self.radius = 10
+        self.x = x - self.radius
+        self.y = y - 50
+        self.rect = Rect(self.x,self.y,self.radius*2,self.radius*2)
+        self.x_speed = 4
+        self.y_speed = -4
+        self.max_speed = 5
+        self.game_over = 0
+
+    def motion(self):
+        collision_threshold = 5
+        block_object = Block.bricks
+        brick_destroyed = 1
+        count_row = 0
+        for row in block_object:
+            count_item = 0
+            for item in row:
+                if self.rect.colliderect(item[0]):
+                    if abs(self.rect.bottom - item[0].top) < collision_threshold and self.y_speed > 0:
+                        self.y_speed *= -1
+                    if abs(self.rect.top - item[0].bottom) < collision_threshold and self.y_speed < 0:
+                        self.y_speed *= -1
+                    if abs(self.rect.right - item[0].left) < collision_threshold and self.x_speed > 0:
+                        self.xspeed *= -1
+                    if abs(self.rect.left - item[0].right) < collision_threshold and self.x_speed < 0:
+                        self.x_speed *= -1
+                    if block_object[count_row][count_item][i] > 1:
+                        block_object[count_row][count_item][1] -= 1
+                    else:
+                        block_object[count_row][count_item][0] = (0,0,0,0)
+
