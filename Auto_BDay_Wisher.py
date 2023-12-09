0b5ecba2b0a861959159ec98389c1852cad995bf
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 16:15:51 +0545

    Auto_BDay_Wisher

--- Auto_BDay_Wisher.py
+++ Auto_BDay_Wisher.py
@@ -16,3 +16,22 @@ def sendEmail(to,sub,msg):
     s.login(GMAIL_ID,GMAIL_PSWD)
     s.sendmail(GMAIL_ID,to,f"Subject: {sub} \n\n {msg}")
     s.quit()
+
+if __name__ == "__main__":
+    df = pd.read_excel("data.xlsx")
+    today = datetime.datetime.now().strftime("%d-%m")
+    yearNow = datetime.datetime.now().strftime("%Y")
+    writeInd = []
+    for index,item in df.iterrows():
+        bday = item['Birthday']
+        bday = datetime.datetime.strptime(bday,"%d-%m-%Y")
+        bday = bday.strftime("%d-%m")
+        if(today == bday) and yearNow not in str(item['LastWishedYear']):
+            sendEmail(item['Email'],"Happy Birthday",item['Dialogue'])
+            writeInd.append(index)
+
+    if writeInd != None:
+        for i in writeInd:
+            oldYear = df.loc[i,'LastWishedYear']
+            df.loc[i,'LastWishedYear'] = str(oldYear) + "," + str(yearNow)
+    df.to_excel('data.xlsx',index=False)
