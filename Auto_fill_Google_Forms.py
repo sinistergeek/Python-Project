679a6cb76c67f5def9669a7a40e29ec3c43a8641
sinistergeek <susanbhattraidon@gmail.com>
2023-12-09 18:19:31 +0545

    Auto_fill_Google_Forms

--- Auto_fill_Google_Forms.py
+++ Auto_fill_Google_Forms.py
@@ -8,3 +8,28 @@ inputEmailID = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[
 inputPhone = '/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
 
 Submit = '/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div/div'
+
+browswer = webdriver.Firefox(executable_path='C:\geckodriveer-v0.28.0-win64\geckodriver.exe')
+browswer.get('https://docs.google.com/forms/d/e/1FAIpQLScBejWF809oacjZlvkciXREi50fyHcq751988KDJo3ycG7xkg/viewform')
+name = []
+email = []
+phone = []
+
+with open("input.csv","r") as f_input:
+    csv_input = csv.DictReader(f_input)
+    for row in csv_input:
+        name.append(row['name'])
+        email.append(row['email'])
+        phone.append(row['phone_number'])
+i=0
+while i < len(name):
+    browser.find_element_by_xpath(inputName).send_keys(name[i])
+    browser.find_element_by_xpath(inputEmailID).send_keys(email[i])
+    browser.find_element_by_xpath(inputPhone).send_keys(phone[i])
+    sleep()
+    browser.find_element_by_xpath(Submit).click()
+    i+=1
+    sleep()
+    browswer.back()
+    sleep()
+browswer.quit()
