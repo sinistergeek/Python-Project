1b3f0464826d96fd42632a6dfc38250bc0bc2b6f
sinistergeek <susanbhattraidon@gmail.com>
2023-12-10 12:28:15 +0545

    Bubble_shooter_game

---
+++ Bubble_shooter_game.py
@@ -0,0 +1,17 @@
+import math
+import pygame
+import copy
+import time
+import sys
+import os
+import random
+import pygame.gfxdraw
+from pygame.locals import *
+
+FPS = 120
+winwdth = 940
+winhgt = 740
+txthgt = 20
+bubblerad = 20
+bubblewdth = bubblerad * 2
+
