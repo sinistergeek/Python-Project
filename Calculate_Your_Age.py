6aa823ecc77f19ace5d4f9391836578441c0b4ff
sinistergeek <susanbhattraidon@gmail.com>
2023-12-11 08:00:10 +0545

    Calculate_Your_Age

--- Calculate_Your_Age.py
+++ Calculate_Your_Age.py
@@ -34,3 +34,10 @@ for y in range(begin_year,end_year):
         day = day + 366
     else:
         day = day + 365
+
+leap_year = judge_leap_year(localtime.tm_year)
+for m in range(1,localtime.tm_mon):
+    day = day + month_days(m,leap_year)
+day = day + localtime.tm_mday
+print("%s age is %d years or"%(name,year),end="")
+print("%d months or %d days"%(month,day))
