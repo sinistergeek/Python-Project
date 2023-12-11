32e3f9dc22b43698575d4bba3767dfe30ed77336
sinistergeek <susanbhattraidon@gmail.com>
2023-12-11 15:46:38 +0545

    Calendar_GUI

--- Calendar_GUI.py
+++ Calendar_GUI.py
@@ -13,5 +13,21 @@ def showCal():
     cal_data = calendar.calendar(find_year)
     cal_year = Label(box,text=cal_data,font="consolas 10 bold",justify=LEFT)
     cal_year.grid(row=2,column=1,padx=20)
-
     box.mainloop()
+if __name__ == "__main__":
+    gui = Tk()
+    gui.config(background="misty rose")
+    gui.title("CALENDAR")
+    gui.geometry("250x250")
+    cal = Label(gui,text="CALENDAR",bg="lavender",font=("Helvetica",28,'bold','underline'))
+    year = Label(gui,text="Enter Year",bg="peach puff",padx=10,pady=10)
+    year_field = Entry(gui)
+    show = Button(gui,text="Show Calendar",fg="Black",bg="lavender",command=showCal)
+    Exit = Button(gui,text="CLOSE",bg="peach puff",command=exit)
+    cal.grid(row=1,column=1)
+    year.grid(row=3,column=1)
+    year_field.grid(row=4,column=1)
+    show.grid(row=5,column=1)
+    Exit.grid(row=7,column=1)
+    gui.mainloop()
+    
