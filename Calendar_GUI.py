d9dab5215be1c4c59677881c4b6221468cb29a1d
sinistergeek <susanbhattraidon@gmail.com>
2023-12-11 15:33:19 +0545

    Calendar_GUI

--- Calendar_GUI.py
+++ Calendar_GUI.py
@@ -8,3 +8,10 @@ def showCal():
     box.geometry("550x600")
     find_year = int(year_field.get())
     first_label = Label(box,text='CALENdAR', bg='dark grey', font=("times",28,'bold'))
+    first_label.grid(row=1,column=1)
+    box.config(background="white")
+    cal_data = calendar.calendar(find_year)
+    cal_year = Label(box,text=cal_data,font="consolas 10 bold",justify=LEFT)
+    cal_year.grid(row=2,column=1,padx=20)
+
+    box.mainloop()
