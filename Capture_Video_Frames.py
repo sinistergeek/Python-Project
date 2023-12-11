37803512d8754dc8dba7168886d9740b61331146
sinistergeek <susanbhattraidon@gmail.com>
2023-12-11 20:16:45 +0545

    Capture_Video_Frames

--- Capture_Video_Frames.py
+++ Capture_Video_Frames.py
@@ -11,3 +11,18 @@ class FrameCapture:
         if os.path.exists(self.directory):
             shutil.rmtree(self.directory)
         os.mkdir(self.directory)
+
+    def capture_frame(self):
+        cv2_object = cv2.VideoCapture(self.file_path)
+        frame_number =0
+        frame_found = 1
+        while frame_found:
+            frame_found,image =cv2_object.read()
+            capture = f'{self.directory}/frame{frame_number}.jpg'
+            cv2.imwrite(capture,image)
+            frame_number += 1
+            
+if __name__ == '__main__':
+    file_path =sys.argv[1]
+    fc = FrameCapture(file_path)
+    fc.capture_frames()
