09cbc84247e54b5fa3de57e9af868ea975c8b488
sinistergeek <susanbhattraidon@gmail.com>
2023-12-12 17:16:48 +0545

    todo

--- todo.py
+++ todo.py
@@ -12,3 +12,30 @@ def todo(ctx):
 @todo.command()
 @click.pass_context
 
+def tasks(ctx):
+    if ctx.obj['TASKS']:
+        click.echo('YOUR TASKS\n************')
+        for i,task in ctx.obj['TASKS'].items():
+            click.echo('.'+task+'(ID:'+i+')')
+        click.echo('')
+    else:
+        click.echo('No tasks yet! Use ADD to add one.\n')
+
+@todo.command()
+@click.pass_context
+@click.option('-add','--add_task',prompt='Enter task to add')
+
+def add(ctx,add_task):
+    if add_task:
+        ctx.obj['TASKS'][ctx.obj['LATEST']] = add_task
+        click.echo('Added task"'+ add_task +'"with ID' + str(ctx.obj['LATEST']))
+        curr_ind = [str(ctx.obj['LATEST'] + 1)]
+        tasks = [str(i) + '```' + t for (i,t) in ctx.obj['TASKS'].items()]
+        with open('./todo.txt','w') as f:
+            f.writelines(['%s\n'%en for en in curr_ind + tasks])
+
+@todo.command()
+@click.pass_context
+@click.option('-fin','--fin_taskid',prompt='Enter ID of task to finish',type=int)
+
+
