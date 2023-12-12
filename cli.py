ba7bb451d7a91d6cace2b6d934494cbcb4034391
sinistergeek <susanbhattraidon@gmail.com>
2023-12-12 10:00:25 +0545

    cli

--- cli.py
+++ cli.py
@@ -8,4 +8,25 @@ def valid_proxy(ctx,param,value):
     validator = re.compile(r'(https|http|socks4|socks5):\/\/'
                            r'((?:[0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{2,5})?'
                            r'|([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)')
+    if not validator.match(value):
+        raise click.BadParameter('Please provide a proxy in the format' 'type://address(eg,https://42.32.23.33)')
+    else
+    return value
+
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('proxy',callback=validate_proxy)
+@click.option('--iptest',default='iptest.ingokleiber.de',help='iptest address')
+@click.option('--csv',default='proxies.csv',help='CSV path')
+
+def single(proxy,iptest,csv):
+    test_single_proxy(proxy,iptest,csv)
+
+@click.command()
+@click.argument('csv')
+@click.option('--iptest',default='iptest.ingokeiber.de',help='iptest address')
 
