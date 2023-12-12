e57e6556e41a6139fafacb1235a104651210873b
sinistergeek <susanbhattraidon@gmail.com>
2023-12-12 20:00:25 +0545

    PDF_to_TXT_converter

---
+++ PDF_to_TXT_converter.py
@@ -0,0 +1,19 @@
+import PyPDF2
+import os
+
+if(os.path.isdir('temp') == False):
+    os.mkdir('temp')
+
+txtpath = ''
+pdfpath = ''
+
+pdfpath = input('Enter the name of your pdf file - please use backslash when typing in directory path:')
+txtpath = input('Enter the name of your txt file - please use blackslash when typing in directory path:')
+BASEDIR = os.path.realpath('temp')
+print(BASEDIR)
+
+if(len(txtpath)==0):
+    txtpath=os.path.join(BASEDIR,os.path.basename(os.path.normpath(pdfpath)).replace(".pdf","")+".txt")
+    pdfobj = open(pdfpath,'rb')
+    pdfread =PyPDF2.PdfFileReader(pdfobj)
+    
