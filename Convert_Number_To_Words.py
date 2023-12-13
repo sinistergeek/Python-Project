2f50f4fadd2888c685b9f0d36956605e386650f7
sinistergeek <susanbhattraidon@gmail.com>
2023-12-13 20:05:46 +0545

    Convert_Number_To_Words

--- Convert_Number_To_Words.py
+++ Convert_Number_To_Words.py
@@ -45,3 +45,35 @@ def converter(n):
                         else:
                             number = one_digit_words[num[1][1 if int(num[1]) in range(3,6,2) else 0]]
                             word.append(number +("teen" if not number[-1] == "t" else "een"))
+                    else:
+                        word.append(one_digit_words[num[0][1]][1 if int(num[0]) in range(2,6) else 0] + ("ty" if num[0] != '8' else 'y') + (one_digit_words[num[1]][0] if num[1] != '0' else ""))
+                        break
+                else:
+                    num = num[1:]
+                    continue
+
+            if len(num) == 3:
+                if num[0] != '0':
+                    word.append(one_digit_words[num[0]][0] + ""+hundred)
+                    if num[1:] == '00':break
+                num=num[1:]
+
+        if len(sum_list[i:] > 1 and not skip):
+            word.append(large_sum_words[len(sum_list[i:]) -2])
+            skip = True
+
+    word = "".join(map(str.strip,word))
+
+    return word[0].lstrip().upper()+word[1:].rstrip().lower() if "negative" not in word else word[:11].lstrip() + word[11].upper()+word[12:].rstrip().lower()
+
+if __name__ == "__main__":
+    while True:
+        try:
+            n = input("Enter any number to convert it into word or 'exit' to stop:")
+            if n == "exit":
+                break
+            int(n)
+            print(n,"-->",converter(n))
+
+        except ValueError:
+            print("Error: Invalid Number!")
