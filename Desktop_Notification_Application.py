4057306e4401d670273008c8fabeb3f83944c3df
sinistergeek <susanbhattraidon@gmail.com>
2023-12-15 08:26:20 +0545

    Desktop_Notification_Application

---
+++ Desktop_Notification_Application.py
@@ -0,0 +1,7 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(title="Alert!!",message="Take a break! It has been an hour!",timeout=10)
+        time.sleep(3600)
