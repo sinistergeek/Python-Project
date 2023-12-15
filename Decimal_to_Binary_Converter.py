989256d987bc457d9dfd2c09ca3f70c6261e9e1e
sinistergeek <susanbhattraidon@gmail.com>
2023-12-15 08:24:15 +0545

    Decimal_to_Binary_Converter

---
+++ Decimal_to_Binary_Converter.py
@@ -0,0 +1,15 @@
+try:
+    menu = int(input("Choose an option: \n 1. Decimal to binary \n 2. Binary to decimal \n Option:"))
+    if menu < 1 or menu > 2:
+        raise ValueError
+
+    if menu == 1:
+        dec = int(input("Input you decimal number: \n Decimal:"))
+        print("Binary: {}".format(bin(dec)[2:]))
+
+    elif menu == 2:
+        binary = input("Input your bnary number:\n Binary:")
+        print("Decimal: {}".format(int(binary,2)))
+
+    except ValueError:
+        print("please choose a valid option")
