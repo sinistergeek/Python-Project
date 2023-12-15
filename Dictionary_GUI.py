5dc15438e5f18d33eeca61668448106963d8d159
sinistergeek <susanbhattraidon@gmail.com>
2023-12-15 10:09:23 +0545

    Dictionary_GUI

--- Dictionary_GUI.py
+++ Dictionary_GUI.py
@@ -24,3 +24,17 @@ def getMeaning():
     else:
         messagebox.showinfo("Error","Please add a Noun,Pronoun,verb or a valid word.")
     meaning_label.config(text=meaning)
+
+heading_label = Label(root,text="DICTIONARY",font=("Helvetica 35 blod"),foreground='Blue')
+heading_label.config(anchor=CENTER)
+heading_label.pack(pady=10)
+
+frame = Frame(root)
+Label(frame,text="Enter Word",font=("Helvetica 15 bold")).pack(side=LEFT)
+word = Entry(frame,font=("Helvetica 15 bold"))
+word.pack(padx=10)
+frame.pack()
+
+search_button = Button(root,text="Search Word",font=("Helvetica 15 bold"),relif=RIDGE,borderwidth=3,cursor="hand2",foreground='Green',command=getMeaning)
+search_button.config(anchor=CENTER)
+search_button.pack(pady=10)
