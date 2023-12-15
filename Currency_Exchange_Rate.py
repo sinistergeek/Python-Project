7848ae3c377c8680b1c51bdeba8d7ceef084916b
sinistergeek <susanbhattraidon@gmail.com>
2023-12-15 08:20:29 +0545

    Currency_Exchange_Rate

--- Currency_Exchange_Rate.py
+++ Currency_Exchange_Rate.py
@@ -17,6 +17,21 @@ for option in options:
     current_element = {'name':currency_name,'short':currency_short}
     currencies.append(current_element)
     print('{}.{}({})'.format(len(currencies),current_element['name'],current_element['short']))
-    currency_index = int(input('Enter your currency\'s position number:'))
-    currency = currencies[currency_index]
+currency_index = int(input('Enter your currency\'s position number:'))
+currency = currencies[currency_index]
+amount = input('\033c Enter amount of {}s (if amount isn\'t integer,then write it with a dot, not comma):').format(currency['name'].lower())
+currencies_table_url = 'https://www.x-rates.com/table/?from={}&amount={}'.format(currency['short'],amount)
+currencies_table_page = req.get(currencies_table_url).text
+soup=BeautifulSoup(currencies_table_page,'html.parser')
+table_rows = soup.findChild('table',attrs={'class':'tablesorter'}).findChildren('tr')[1:]
+print('\033cFor {} {}s youll get'.format(amount,currency['name'].lower()))
+
+for table_row in table_rows:
+
+    row_data = table_row.findChildren('td')
+    exchange_rate={
+    'currency':row_data[0].text,
+    'amount':float(row_data[1].text)
+            }
+    print('{:.3f} {}s'.format(exchange_rage['amount'],exchange_rate['currency']))
 
