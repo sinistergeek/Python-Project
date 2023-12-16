46a757729f5c2eede8ff64e1448d8377607cfe2e
sinistergeek <susanbhattraidon@gmail.com>
2023-12-16 19:47:00 +0545

    Finding_Dominant_Color

--- Finding_Dominant_Color.py
+++ Finding_Dominant_Color.py
@@ -17,4 +17,11 @@ ocurrance = dct(zip(unique,counts))
 a1_sorted_keys = sorted(ocurrance,key=ocurrance.get,reverse=True)
 print(a1_sorted_keys[:3])
 image = np.zeros((300,300,3),np.uint8)
-
+image[:] = a1_sorted_keys[:3]
+c=a1_sorted_keys[0]
+color =np.zeros((300,300,3),np.uint8)
+color[:] = (c,c,c)
+print("Tone :" + str(a1_sorted_keys[:3]))
+cv2.imshow("Tone",image)
+print("color:"+str([c,c,c]))
+cv2.imshow("color",color)
