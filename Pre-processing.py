9f0e036debdc8a6e64d1b35ed9b7f146890e1a87
sinistergeek <susanbhattraidon@gmail.com>
2023-12-16 16:18:50 +0545

    Pre-processing

---
+++ Pre-processing.py
@@ -0,0 +1,19 @@
+import spacy
+import neologdn
+class EnglishCorpus:
+
+    def __init__(self):
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def preprocessing(self,text:str) -> str:
+        text = text.replace("\n","")
+        text = neologdn.normalize(text)
+
+        return text
+
+    def make_sentence_list(self,sentences:str) -> list:
+        doc = self.nlp(sentences)
+        self.ginza_sents_object = doc.sents
+        sentence_list = [s for s in doc.sents]
+
+        return sentence_list
