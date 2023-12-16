c4bec5d19933f66ea209ecff0d5dc1b0769c6ecf
sinistergeek <susanbhattraidon@gmail.com>
2023-12-16 15:53:47 +0545

    Discord_Bot

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -14,4 +14,26 @@ async def on_ready():
 
 async def on_memeber_join(member):
     welcome_channel = discord.utils.get(member.guild.channels,name=WELCOME_CHANNEL)
+    await welcome_channel.send(f"welcome {member.mention}, please read our rules and have a great time!")
+
+@commands.has_permissions(ban_members=True)
+@bot.command()
+async def ban(ctx,user:discord.Member):
+    await ctx.guild.ban(user,delete_message_days=0)
+    await ctx.send(f"banned {user}")
+
+
+@commands.has_permissions(ban_members=True)
+@bot.command()
+async def unban(ctx,user:discord.User):
+    await ctx.guild.unban(user)
+    await ctx.send(f"unbanned {user}")
+
+
+@commands.has_permissions(kick_members=True)
+@bot.command()
+async def kick(ctx,user:discord.User):
+    await ctx.guild.kick(user)
+    await ctx.send(f"kicked {user}")
+
 
