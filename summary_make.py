343ef22f7a2bd585e7002c85492e6d655604e7b1
sinistergeek <susanbhattraidon@gmail.com>
2023-12-16 16:23:25 +0545

    summary_make

---
+++ summary_make.py
@@ -0,0 +1,7 @@
+from preprocessing import EnglishCorpus
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.utils import get_stop_words
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+
