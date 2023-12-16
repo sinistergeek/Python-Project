44277f4a0eccc881a54aedebbedeead6b71d796a
sinistergeek <susanbhattraidon@gmail.com>
2023-12-16 19:59:29 +0545

    Download_website_Articles_as_pdf

--- Download_website_Articles_as_pdf.py
+++ Download_website_Articles_as_pdf.py
@@ -12,3 +12,20 @@ def get_driver():
     chrome_options.add_argument("--kiosk-printing")
     browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(),options=chrome_options)
     return browser
+
+def download_article(URL):
+    browser = get_driver()
+    browser.get(URL)
+    browser.execute_script("window.print();")
+    browser.close()
+
+if __name__ == "__main__":
+    URL = input("provide article URL:")
+    if requests.get(URL).status_code == 200:
+        try:
+            download_article(URL)
+            print("Your article is successfully downloaded")
+        except Exception as e:
+            print(e)
+    else:
+        print("Enter a valid working URL")
