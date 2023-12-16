6fbd859199939ac5c192b03128bd5ffc80fae416
sinistergeek <susanbhattraidon@gmail.com>
2023-12-16 20:16:55 +0545

    Scrap_images_from_URL

--- Scrap_images_from_URL.py
+++ Scrap_images_from_URL.py
@@ -8,3 +8,29 @@ path = input("Enter Path:")
 url = input("Enter URL:")
 output = "output"
 
+def get_url(path,url):
+    driver = webdriver.Chrome(executable_path=r"{}".format(path))
+    driver.get(url)
+    print("loading.....")
+    res = driver.execute_script("return document.documentElement.outerHTML")
+    return res
+def get_img_links(res):
+    soup = BeautiifulSoup(res,"lxml")
+    imglinks = soup.find_all("img",src=True)
+    return imglinks
+
+def download_img(img_link,index):
+    try:
+        extensions = [".jpeg",".jpg",".png",".gif"]
+        extension = ".jpg"
+        for exe in extensions:
+            if img_link.find(exe) > 0:
+                extension = exe
+                break
+            imag_data = rq.get(img_link).content
+            with open(output + "\\" + str(index + 1) + extension, "wb+") as f:
+                f.write(img_data)
+                f.close()
+    except Exception:
+        pass
+
