a41fc5e97ff05dc272c9f040c1cce0b01956a333
sinistergeek <susanbhattraidon@gmail.com>
2023-12-17 15:35:54 +0545

    Email_GUI_Functionalities

--- Email_GUI_Functionalities.py
+++ Email_GUI_Functionalities.py
@@ -17,3 +17,18 @@ def send_message():
     password_entry.delete(0,END)
     sender_address_entry.delete(0,END)
 
+gui = Tk()
+gui.geometry("500x500")
+gui.titl("Email Sender App")
+heading = Label(text="Email Sender App",bg="yellow",fg="black",font="10",width="500",height="3")
+heading.pack()
+gui.configure(background="light blue")
+sender_address_field = Label(text="Sender's Email :")
+sender_address_field.place(x=15,y=70)
+sender_address = StringVar()
+sender_address_entry = Entry(textvariable = sender_address,width="30")
+sender_address_entry.place(x=15,y=100)
+sender_password_field = Label(text="Sender's Password:")
+sender_password_field.place(x=15,y=140)
+password = StringVar()
+
