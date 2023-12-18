6063131f39ce2967f2fa6b0b2935acabba48cc07
sinistergeek <susanbhattraidon@gmail.com>
2023-12-18 17:14:05 +0545

    Current_Weather

--- Current_Weather.py
+++ Current_Weather.py
@@ -6,4 +6,15 @@ complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 response = requests.get(complete_url)
 x=response.json()
 
+if x["cod"] != "404":
+    y = x["main"]
+    current_temperature = y["temp"]
+    current_pressure = y["pressure"]
+    current_humidiy = y["humidity"]
+    z = x["weather"]
+    weather_description = z[0]["description"]
+    print("Temperature (in kelvin unit) = " + str(current_temperature) + "\n atmospheric pressure (in hPa unit) =" + str(current_ppressure) + "\n humidity (in percentage) = "+ str(current_humidiy) + "\n description = " + str(weather_description))
 
+
+else:
+    print("City Not Found")
