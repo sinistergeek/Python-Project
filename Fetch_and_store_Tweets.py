883b60b147165e88f218da2180d74cc005b57dc8
sinistergeek <susanbhattraidon@gmail.com>
2023-12-18 17:02:23 +0545

    Fetch_and_store_Tweets

--- Fetch_and_store_Tweets.py
+++ Fetch_and_store_Tweets.py
@@ -12,3 +12,7 @@ auth.set_access_token(access_token,access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 csvFile = open('tweets.csv','a')
 csvWriter = csv.writer(csvFile)
+
+for tweet in tweepy.Cursor(api.search,q=hastag,count=100,lang="en",sinc="2017-04-02").items():
+    print(tweet.created_at,tweet.text)
+    csvWriter.writerow([tweet.created_at,tweet.text.encode('utf-8')])
