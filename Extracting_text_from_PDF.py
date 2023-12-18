8bb71a440acbd6a433f72b03cfcbf61c47db87db
sinistergeek <susanbhattraidon@gmail.com>
2023-12-18 10:26:41 +0545

    Extracting_text_from_PDF

--- Extracting_text_from_PDF.py
+++ Extracting_text_from_PDF.py
@@ -18,3 +18,35 @@ def delete_ppms():
 pdf_files = []
 docx_files = []
 
+for f in os.listdir(PATH):
+    full_name = os.path.join(PATH,f)
+    if os.path.isfile(full_name):
+        name=os.path.basename(f)
+        filename,ext = os.path.slitex(name)
+        if ext == '.pdf':
+            pdf_files.append(name)
+        elif ext == ('.docx'):
+            docx_files.append(name)
+
+def pdf_extact(file,i):
+    print("extacting from file.",file)
+    delete_ppms()
+    images = pdf2image.convert_from_path(PATH + file,output_folder = PATH)
+    j=0
+    for file in sorted(os.listdir(PATH)):
+        if '.ppm' in file and 'image' not in file:
+            os.rename(PATH + file,PATH + 'image'+str(i)+'-'+str(j)+'.ppm')
+            j += 1
+    j=0
+    f = open(PATH + 'result{}.txt'.format(i),'w')
+    files = [f for f in os.listdir(PATH) if '.ppm' in f]
+    for file in sorted(files,key=lambda x: int(x[x.index('-') + 1: x.index('.')])):
+        temp = pytesseract.image_to_string(Image.open(PATH + file))
+        f.write(temp)
+    f.close()
+
+for i in range(len(pdf_files)):
+    pdf_file = pdf_files[i]
+
+
+
