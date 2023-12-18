d63d40be8d9f39457189b10809eb0e9701e19764
sinistergeek <susanbhattraidon@gmail.com>
2023-12-18 15:47:38 +0545

    Facebook_Profile_Picture_Downloader

--- Facebook_Profile_Picture_Downloader.py
+++ Facebook_Profile_Picture_Downloader.py
@@ -5,3 +5,12 @@ path = os.getcwd()
 
 if not "fb_dps" in os.listdir(path):
     os.mkdir("fb_dps")
+
+fbid = int(input("Enter the Facebook-id to download it's profile picture:"))
+try:
+    result = requests.get(url.format(fbid))
+    with open("fb_dps/{}_img.jpg".format(fbid),"wb") as file:
+        file.write(result.content)
+
+except:
+    print("There was some error")
