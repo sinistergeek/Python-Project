1dd509c1e6e5054e0d93ad811649ff7104244eb3
sinistergeek <susanbhattraidon@gmail.com>
2023-12-18 09:34:43 +0545

    Encrypt_and_decrypt_text

--- Encrypt_and_decrypt_text.py
+++ Encrypt_and_decrypt_text.py
@@ -9,4 +9,11 @@ iv = Random.new().read(AES.block_size)
 mycipher = AES.new(key,AES.MODE_CFB,iv)
 ciphertext = iv + mycipher.encrypt(plain_text.encode())
 mydecrypt = AES.new(key,AES.MODE_CFB,ciphertext[:16])
-
+ciphertext
+file_out = open("encrypted.bin","wb")
+file_out.write(ciphertext[16:])
+file_out.close()
+print("The key k is: ",key)
+print("iv is: ",b2a_hex(ciphertext)[:16])
+print("The encrypted data is:",b2a_hex(ciphertext)[16:])
+print("The decrypted data is:",decrypttext.decode())
