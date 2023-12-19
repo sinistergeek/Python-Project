affe71cea20a25f2a42733161236ef152d0444b1
sinistergeek <susanbhattraidon@gmail.com>
2023-12-19 10:20:38 +0545

    Flames_Games

--- Flames_Games.py
+++ Flames_Games.py
@@ -12,5 +12,25 @@ def tell_status():
     p2 = p2.replace("","")
     p1 = list(p1)
     p1 = list(p2)
-
     Status.field.insert(10,result_flame(p1,p2))
+
+def result_flame(x,y):
+    for i in x[:]:
+        if i in y:
+            x.remove(i)
+            y.remove(i)
+    count = len(x) + len(y)
+    result = ["Friends","Love","Affection","Marriage","Enemy","Siblings"]
+    while len(result) > 1:
+        split_index = (count % len(result) - 1)
+        if(split_index >= 0):
+            right = result[split_index + 1:]
+            left =result[:split_index]
+            result = right + left
+        else:
+            result = result[:len(result) -1]
+
+    return result
+
+if __name__ == "__main__":
+
