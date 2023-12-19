4c6211d03f5db88e325751e00d429e41f951cd02
sinistergeek <susanbhattraidon@gmail.com>
2023-12-19 09:50:35 +0545

    IMDB_Rating

--- IMDB_Rating.py
+++ IMDB_Rating.py
@@ -16,3 +16,16 @@ for film in filmswe:
 
 for line in films:
     title = line.lower()
+    query = "+".join(title.split())
+    URL = "https://www.imdb.com/search/title/?title="+query
+    print(URL)
+    try:
+        response =s.get(URL)
+        content = response.content
+        soup = BeautifulSoup(response.content,features="html.parser")
+        containers = soup.find_all("div",class_="lister-item-content")
+        for result in containers:
+            name1 = result.h3.a.text
+            name = result.h3.a.text.lower()
+            if title in name:
+                rating = result.find("div",class_="inline-block ratings-imdb-rating")
