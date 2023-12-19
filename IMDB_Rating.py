9d1e8c138f4e57cffda3470ff573feea1cf57708
sinistergeek <susanbhattraidon@gmail.com>
2023-12-19 09:37:47 +0545

    IMDB_Rating

--- IMDB_Rating.py
+++ IMDB_Rating.py
@@ -3,3 +3,16 @@ import requests
 import pandas as pd
 import os
 
+s = requests.session()
+films = []
+names = []
+ratings = []
+genres = []
+
+path = input("Enter the path where your films are:")
+filmswe = os.listdir(path)
+for film in filmswe:
+    films.append(os.path.splitex(film)[0])
+
+for line in films:
+    title = line.lower()
