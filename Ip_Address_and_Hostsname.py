463bf7623d2bf0799e9be6e49b46164437609587
sinistergeek <susanbhattraidon@gmail.com>
2023-12-19 10:02:59 +0545

    Ip_Address_and_Hostsname

---
+++ Ip_Address_and_Hostsname.py
@@ -0,0 +1,10 @@
+import socket
+
+def get_hostname_IP():
+    hostname = input("Please enter website address(URL):")
+    try:
+        print(f'Hostname:{hostname}')
+        print(f'IP:{socket.gethostbyname(hostname)}')
+    except socket.gaierror as error:
+        print(f'Invalid Hostname,error raised is {error}')
+get_hostname_IP()
