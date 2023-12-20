26315bc6ed4c0873e4975c3496a20dab6ccea812
sinistergeek <susanbhattraidon@gmail.com>
2023-12-20 20:46:06 +0545

    author_utisl

--- author_utisl.py
+++ author_utisl.py
@@ -10,3 +10,21 @@ ERROR_FILE_NOT_FOUND = 0x0002
 ERROR_PATH_NOT_FOUND = 0x0003
 ERROR_ACCESS_DENIED = 0x0005
 ERROR_SHARING_VIOLATION = 0x0020
+SE_FILE_OBJECT = 1
+OWNER_SECURITY_INFORMATION = 0x00000001
+GROUP_SECURITY_INFORMATION = 0x00000002
+DACL_SECURITY_INFORMATION = 0x00000004
+SACL_SECURITY_INFORMATION = 0x00000008
+LABEL_SECURITY_InfORMATION = 0x0000010
+
+_DEFAULT_SECURITY_INFORMATION =(OWNER_SECURITY_INFORMATION|GROUP_SECURITY_INFORMATION|DACL_SECURITY_INFORMATION|LABEL_SECURITY_INFORMATION)
+LPDWORD =ctypes.POINTER(wintypes.DWORD)
+SE_OBJECT_TYPE =wintypes.DWORD
+SECURITY_INFORMAtIOn =wintypes.DWORD
+class SID_NAME_USE(wintypes.DWORD):
+    _sid_types = dict(enumerate("User Group DOmain Alias WellKnownGroup DeletedAccount Invalid Unknown Computer Label".split(),1))
+    def __init(self,value=None):
+        if value is not None:
+            if value not in self.side_types:
+                raise ValueError('invalid SID type')
+            wintypes.DWORD.__init__(value)
