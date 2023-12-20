b14a4b51fb6e8f48ff24a5691ef91ad7b61d87fc
sinistergeek <susanbhattraidon@gmail.com>
2023-12-20 17:03:19 +0545

    get_meta_information_of_images

---
+++ get_meta_information_of_images.py
@@ -0,0 +1,8 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+from author_utils import get_file_security,get_author
+from gps_utils import get_location
+import os
+import sys
+from datetime import datetime
+
