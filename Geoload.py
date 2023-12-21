f0981b823e589294a8dd2fd3bbfba81a7f0986af
sinistergeek <susanbhattraidon@gmail.com>
2023-12-21 11:02:11 +0545

    Geoload

--- Geoload.py
+++ Geoload.py
@@ -12,4 +12,20 @@ if api_key is False:
     serviceurl = "http://py4e-data.dr-chuck.net/json?"
 else:
     serviceurl = "https://maps.googleapis.com/maps/api/geocode/json?"
+conn = sqlite3.connect('geodata.sqlite')
+cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS Locations (address TEXT,geodataTEXT)''')
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+fh = open("where.data")
+count = 0
+for line in fh:
+    if count > 200:
+        print('Retreved 200 locations,restart to retrieve more')
+        break
+    address = line.strip()
+    print('')
+    cur.execute("SELECT geodata FROM Locations WHERE address=?",(memoryview(address.encode()),))
 
