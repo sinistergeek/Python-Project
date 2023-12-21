c3a712451761685bf11499fa418713a2493719b4
sinistergeek <susanbhattraidon@gmail.com>
2023-12-21 10:10:47 +0545

    Wifi_Password_Ejector

--- Wifi_Password_Ejector.py
+++ Wifi_Password_Ejector.py
@@ -1,6 +1,11 @@
 import subprocess
 
-data = (subprocess.check_out(["netsh","wlan","show","profiles"]).decode("utf-8").split("\n"))
+data = (subprocess.check_output(["netsh","wlan","show","profiles"]).decode("utf-8").split("\n"))
 profile=[i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 for i in profiles:
     result=(subprocess.check_output(["netsh","wlan","show","profile",i,"key=clear"]).decode("utf-8").split("\n"))
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        print("{:<30}|{:<}".format(i,results[0]))
+    except:
+        print("{:<30}| {:<}".format(i,""))
