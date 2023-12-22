639bd0c71d85ac2cf5e773510c949fde18142500
sinistergeek <susanbhattraidon@gmail.com>
2023-12-22 13:29:39 +0545

    Detector

--- Detector.py
+++ Detector.py
@@ -7,4 +7,16 @@ def detector():
     face_cascade = cv2.CascadeClassifier(os.path.dirname(sys.argv[0]) + '/haarcascade_frontalface_alt2.xml')
     while True:
         ret,frame=cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(frame,1.3,5)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+            send_mail(frame=frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1)& 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+detector()
 
