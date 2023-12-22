6961365939c1ed205abf00fe08d5334c5b9b8efc
sinistergeek <susanbhattraidon@gmail.com>
2023-12-22 09:34:18 +0545

    HangMan_Game

---
+++ HangMan_Game.py
@@ -0,0 +1,9 @@
+import pygame
+import math
+import random
+pygame.init()
+WIDTH,HEIGHT =800,500
+win = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Hangman Game")
+radius = 20
+gap = 15
