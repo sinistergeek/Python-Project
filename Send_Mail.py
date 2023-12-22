eb61c335c1d0ba7d9a95abc32341947748ebaeb8
sinistergeek <susanbhattraidon@gmail.com>
2023-12-22 16:36:08 +0545

    Send_Mail

--- Send_Mail.py
+++ Send_Mail.py
@@ -11,3 +11,19 @@ import cv2
 def send_mail(frame):
     path = os.path.dirname(sys.argv[0])
     log_file = path + '/email.log'
+    if os.path.isfile(log_file):
+        with open(log_file,'r') as f:
+            date = f.read()
+            date_to_datetime = datetime.strptime(date,"%Y-%m-%d%H:%M:%S.%f")
+            if datetime.now() < date_to_datetime + timedelta(minutes=1):
+                return
+        with open(log_file,'w') as f:
+            f.write(str(datetime.now()))
+
+        cv2.imwrite("intrude.jpg",frame)
+        gmail_user = 'Enter_gmail-ID_to_be_used_for_sending_the_email'
+        gmail_password = 'Enter_gmail-ID_password_here'
+        recipient = 'Enter_gmailID_of_the_recipient'
+        recipient = 'Enter_gmailID_of_the_recipient'
+        message = 'Hey! It appears that someone is at home!!!'
+        msg = MIMEMultipart()
