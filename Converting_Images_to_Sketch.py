12faed8366314873a7925074c0c3bc1499342500
sinistergeek <susanbhattraidon@gmail.com>
2023-12-23 10:08:50 +0545

    Converting_Images_to_Sketch

--- Converting_Images_to_Sketch.py
+++ Converting_Images_to_Sketch.py
@@ -3,3 +3,19 @@ import os
 import os.path
 from datetime import datetime,date
 
+def openCam():
+    datsets = 'webcam'
+    sub_data = 'data'
+    path = os.path.join(datasets,sub_data)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    webcam=cv2.VideoCapture(0,cv2.CAp_DSHOW)
+    (_,im) = webcam.read()
+    now = str(datetime.now())
+    now = now.replace(':','')
+    now = now.replace('-','')
+    now = now.replace('.','')
+    now = now.replace(' ','')
+    cv2.imwrite(f'webcam\\data\\{now}.png'im)
+    img = f'webcam\\data\\{now}.png'
+    convert(img)
