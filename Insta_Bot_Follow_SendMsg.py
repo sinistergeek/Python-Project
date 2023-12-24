cda1a713407637343186c963cca22cc566bc2e31
sinistergeek <susanbhattraidon@gmail.com>
2023-12-24 20:01:10 +0545

    Insta_Bot_Follow_SendMsg

--- Insta_Bot_Follow_SendMsg.py
+++ Insta_Bot_Follow_SendMsg.py
@@ -16,3 +16,27 @@ PASSWORD = input("Enter Your Password")
 browser.get('https://www.instagram.com/')
 wait = WebDriverWait(browser,120)
 time.sleep(2)
+username_field = browser.find_element_by_name('username')
+username_field.send_keys(USERNAME)
+password_field = browser.find_element_by_name('password')
+password_field.send_keys(PASSWORD)
+login_btn = browser.find_element_by_css_selector('button[type="submit"]')
+login_btn.click()
+print(users)
+time.sleep(5)
+for user in users:
+    browser.get(f"https://www.isntagram.com/{user}/")
+    time.sleep(3)
+    try:
+        follow = wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/div[1]/div[1]/div/div/div/span/span[1]/button')))
+        follow.click()
+        time.sleep(3)
+    except:
+        pass
+    try:
+        message = browser.find.element_by_class_name('_862NM')
+        message.click()
+        time.sleep(4)
+        browser.find_element_by_class_name('mt3GC').click()
+
