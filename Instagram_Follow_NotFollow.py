af7c332e995d034ff442300d60cf2458c4485add
sinistergeek <susanbhattraidon@gmail.com>
2023-12-24 20:05:30 +0545

    Instagram_Follow_NotFollow

---
+++ Instagram_Follow_NotFollow.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from time import sleep
+import datetime
+from prettytable import PrettyTable
+
+start =datetime.datetime.now()
+table=PrettyTable()
+column_names = ["Non-Followers"]
+
