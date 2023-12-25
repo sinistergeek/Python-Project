ce1a362337928645c6cc00ed85b6442939d7300e
sinistergeek <susanbhattraidon@gmail.com>
2023-12-25 15:54:20 +0545

    Instagram_Follow_NotFollow

--- Instagram_Follow_NotFollow.py
+++ Instagram_Follow_NotFollow.py
@@ -37,4 +37,20 @@ class InstaBot:
         notfollowingback = [user for user in following if user not in followers]
         table.add_column(column_names[0],notfollowingback)
         print(table)
+    
+    def _get_names(self):
+        sleep(2)
+        scroll_box =self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div[2]')
+        last_ht,ht = 0,1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script("""arguments[0].scrollTo(0,arguments[0].scrollHeight);
+            return arguments[0].scrollHeight;""",scroll_box)
+        links = scroll_box.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text !='']
+        sleep(1)
+        close_btn =  self.driver.find_element_by_xpath('/html/body/div[5]/div/div[1]/div/div[2]')
+        close_btn.click()
+        return names
 
