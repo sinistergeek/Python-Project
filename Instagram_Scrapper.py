675fa74837aa2919320ab7b115b77d3099bb85f3
sinistergeek <susanbhattraidon@gmail.com>
2023-12-26 18:59:05 +0545

    Instagram_Scrapper

--- Instagram_Scrapper.py
+++ Instagram_Scrapper.py
@@ -8,3 +8,9 @@ print("Followers:",profile.followers)
 print("Followees:",profile.followees)
 print("Bio:",profile.biography,profile.external_url)
 bot.login(user="your username",passwd="your password")
+bot.interactive_login("your username")
+followers = [follower.username for follower in profile.get_followers()]
+followees = [followee.username for followee in profile.get_followees()]
+print(followers)
+profile = instaloader.Profile.from_username(bot.context,'wwe')
+posts = profile.get_posts()
