d1d3ae4e0801ec944509d1ccdb24192059dda384
sinistergeek <susanbhattraidon@gmail.com>
2023-12-26 14:51:21 +0545

    Instagram_Liker_Bot

--- Instagram_Liker_Bot.py
+++ Instagram_Liker_Bot.py
@@ -18,3 +18,30 @@ login_button = chrome.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]')
 login_button.click()
 time.sleep(10)
 search_bar = chrome.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+search_bar.send_keys(input("Enter the username of the other person:"))
+time.sleep(7)
+search_bar.send_keys(Keys.ENTER)
+search_bar.send_keys(Keys.ENTER)
+time.sleep(2)
+post = chrome.find_element_by_xpath(
+    '//*[@id="react-root"]/section/main/div/div[3]/article/div[1]/div/div[1]/div[1]/a/div[1]/div[2]')
+post.click()
+time.sleep(2)
+like_button = chrome.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+like_button.click()
+next_button = chrome.find_element_by_xpath('/html/body/div[5]/div[1]/div/div/a')
+next_button.click()
+time.sleep(2)
+while True:
+    try:
+        like_button = chrome.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[1]/span[1]/button')
+        like_button.click()
+        next_button = chrome.find_element_by_path('/html/body/div[5]/div[1]/div/div/a[2]')
+        next_button.click()
+        time.sleep(5)
+
+    except:
+        close_button = chrome.find_element_by_xpath('/html/body/div[5]/div[3]/button')
+        close_button.click()
+        break
+
