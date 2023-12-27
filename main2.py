8a573ba85199ca0b4ac3b029f65a5351409e6f56
sinistergeek <susanbhattraidon@gmail.com>
2023-12-27 18:21:02 +0545

    main2

---
+++ main2.py
@@ -0,0 +1,12 @@
+import requests
+from lxml import html
+import re
+import sys
+import pprint
+from profilepic import pp_download
+
+def banner():
+    print('\t""""""""')
+    print('\t InstagramProfile data graber ')
+    print('\t """"""""""""' ')
+
