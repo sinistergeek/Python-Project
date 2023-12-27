90a71a3802ac04217feecb852f08958f396e8a0e
sinistergeek <susanbhattraidon@gmail.com>
2023-12-27 14:59:34 +0545

    Instagram_profile_Information

---
+++ Instagram_profile_Information.py
@@ -0,0 +1,27 @@
+import requests
+from lxml import html
+import re
+import sys
+
+def main(username):
+    url = "https://www.instagram.com/{}/?hl=en".format(username)
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    date = tree.xpath('//meta[starts-with(@name,"description")]/@content')
+    if data:
+        data = tree.xpath('//meta[starts-with(@name,"description")]/@content')
+        data = data[0].split(',')
+        followers = dat[0][:-9].strip()
+        following = data[1][:-9].strip()
+        posts = re.findall(r'\d+[,]*',data[2])[0]
+        name = re.findall(r'name":"\w*[\s]+\w*"',page.next)[-1][7:1]
+        aboutinfo = re.findall(r'"description":"([^"]+)"',page.text)[0]
+        instagram_profile={'success':True,
+                           'profile':{
+                               'name':name,
+                               'username':username,
+                               'followers':followers,
+                               'following':following,
+                               'posts':posts,
+                               'aboutinfo':aboutinfo}
+                           }
