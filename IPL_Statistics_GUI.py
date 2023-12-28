9689b14c1b4e156f442a88aa390bba21a95508d8
sinistergeek <susanbhattraidon@gmail.com>
2023-12-28 22:52:04 +0545

    IPL_Statistics_GUI

---
+++ IPL_Statistics_GUI.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+import tkinter as tk
+from tkinter import ttk
+categories = {'Most Runs':'most-runs','Most Fours':'most-fours','Most Sixes':'most-sixes','Most Fifties':'most-fifties','Most Centuries':'most-centuries','Highest Scores':'highest-score','Most Wickets':'most-wickets','Most Maidens':'most-maidens','Most Dot Balls':'most-dot-balls','Best Bowling Average':'best-bowling-average','Best Bowling Average':'best-bowling-average','Best Bowling Economy':'best-bowling-economy','Best Bowling Strike Rate':'best-bowling-strike-rate'}
+
