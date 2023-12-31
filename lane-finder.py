3e6e4411ffda2304e524b0d8d26f969f69c519b2
sinistergeek <susanbhattraidon@gmail.com>
2023-12-31 18:16:52 +0545

    lane-finder

--- lane-finder.py
+++ lane-finder.py
@@ -58,4 +58,17 @@ def region_of_interest(canny):
     masked_image = cv2.bitwise_and(canny,mask)
     return masked_image
 
-
+cap = cv2.VideoCapture("test2.mp4")
+while(cap.isOpened()):
+    _,frame = cap.read()
+    canny_image = canny(frame)
+    cropped_canny =region_of_interest(canny_image)
+    lines = cv2.HoughLines(cropped_canny,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame,lines)
+    line_image = display_lines(frame,averaged_lines)
+    combo_image = cv2.addWeighted(frame,0.8,line_image,1,1)
+    cv2.imshow("result",combo_image)
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+        break
+    cap.release()
+    cv2.destroyAllWindows()
