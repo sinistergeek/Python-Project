00014d96e0e336dc723d2a02e785baa37ade158c
sinistergeek <susanbhattraidon@gmail.com>
2023-12-31 08:57:09 +0545

    lane-finder

--- lane-finder.py
+++ lane-finder.py
@@ -14,4 +14,21 @@ def average_slope_intercept(image,lines):
     right_fit = []
     if lines is None:
         return None
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            fit = np.polyfit((x1,x2),(y1,y2),1)
+            slope = fit[0]
+            intercept = fit[1]
+            if slope < 0:
+                left_fit.append((slope,intercept))
+            else:
+                right_fit.append((slope,intercept))
+
+    left_fit_average = np.average(left_fit,axis=0)
+    right_fit_average = np.average(right_fit,axis=0)
+    left_line = make_points(image,left_fit_average)
+    right_line = make_points(image,right_fit_average)
+    averaged_linmes = [left_line,right_line]
+
+    return averaged_lines
 
