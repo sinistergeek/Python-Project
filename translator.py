e1e967aabf2b725b78348e11366c90834e73c968
sinistergeek <susanbhattraidon@gmail.com>
2024-01-01 17:13:19 +0545

    translator

--- translator.py
+++ translator.py
@@ -1,3 +1,23 @@
  from googletans import Translator
  translator =Translator()
- language ={"bn":"Bangla",}
+ language ={"bn":"Bangla",
+            "en":"English",
+            "ko":"Koren",
+            "fr":"French",
+            "de":"German",
+            "he":"Hebrew",
+            "hi":"Hindi",
+            "it":"Italian",
+            "ja":"Japanese",
+            "la":"latin",
+            "ms":"Malay",
+            "ne":"Nepali",
+            "ru":"Russian",
+            "ar":"Arabic",
+            "zh":"Chinese",
+            "es":"Spanish"}
+
+ allow = True
+ while allow:
+     user_code = input(f"Please input desired language code. To see the language code list enter 'options'\n")
+
