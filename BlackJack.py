8312f6be6c9b1a436e999d58a108f98dfe45781e
sinistergeek <susanbhattraidon@gmail.com>
2024-01-04 20:24:52 +0545

    Fixes

--- BlackJack.py
+++ BlackJack.py
@@ -1,6 +1,6 @@
 import random, sys
 HEARTS = chr(9829)
-DIAMONds = chr(9830)
+DIAMONDS = chr(9830)
 SPADES = chr(9824)
 CLUBS = chr(9827)
 BACKSIDE = 'backside'
@@ -95,7 +95,7 @@ def getBet(maxBet):
         if 1 <= bet <= maxBet:
             return bet
 
-def getDuck():
+def getDeck():
     deck = []
     for suit in (HEARTS,DIAMONDS,SPADES,CLUBS):
         for rank in range(2,11):
@@ -140,14 +140,14 @@ def displayCards(cards):
     for i, card in enumerate(cards):
         rows[0] += ' ___ '
         if card == BACKSIDE:
-            row[1] += '|## | '
-            row[2] += '|###| '
-            row[3] += '|_##| '
+            rows[1] += '|## | '
+            rows[2] += '|###| '
+            rows[3] += '|_##| '
         else:
             rank, suit = card
-            row[1] += '|{} | '.format(rank.ljust(2))
-            row[2] += '| {} |'.format(suit)
-            row[3] += '|_{}| '.format(rank.rjust(2,'_'))
+            rows[1] += '|{} | '.format(rank.ljust(2))
+            rows[2] += '| {} |'.format(suit)
+            rows[3] += '|_{}| '.format(rank.rjust(2,'_'))
     for row in rows:
         print(row)
 
