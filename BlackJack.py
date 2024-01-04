9f544beb797743c07ea6b0f01bd652ac1cd1d595
sinistergeek <susanbhattraidon@gmail.com>
2024-01-04 19:55:31 +0545

    BlackJack

--- BlackJack.py
+++ BlackJack.py
@@ -27,3 +27,41 @@ def main():
             print("Good thing weren't playing wiuth real monmey.")
             print('Thanks for playing!')
             sys.exit()
+
+        print('Money:',money)
+        bet = getBet(money)
+        deck = getDeck()
+        dealerHand = [deck.pop(),deck.pop()]
+        playerHand = [deck.pop(),deck.pop()]
+        print('Bet:',bet)
+        while True:
+            displayHands(playerHand,dealerHand,False)
+            print()
+            if getHandValue(playerHand) > 21:
+                break
+            move = getMove(playerHand, money - bet)
+            if move == 'D':
+                additionalBet = getBet(min(bet,(money - bet)))
+                bet += additionalBet
+                print('Bet increased to {}.'.format(bet))
+                print('Bet:',bet)
+
+
+            if move in ('H','D'):
+                newCard = deck.pop()
+                rank, suit = newCard
+                print('You drew a {} of {}.'.format(rank,suit))
+                playerHand.append(newCard)
+                if getHandValue(playerHand) > 21:
+                    continue
+            if move in ('S','D'):
+                break
+        if getHandValue(playerHand) <= 21:
+            while getHandValue(dealerHand) < 17:
+                print('Dealer hits...')
+                dealerHand.append(deck.pop())
+                displayHands(playerHand,dealerHand,False)
+                if getHandValue(dealerHand) > 21:
+                    break
+                input('Press Enter to continue.....')
+
