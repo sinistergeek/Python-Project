e5dc78014296736226e241eafbfe279f871d6953
sinistergeek <susanbhattraidon@gmail.com>
2024-01-05 14:27:03 +0545

    Bouncing_DVD_LOGo

--- Bouncing_DVD_LOGo.py
+++ Bouncing_DVD_LOGo.py
@@ -50,3 +50,46 @@ def main():
                 logo[DIR] = DOWN_LEFT
                 cornerBounces += 1
 
+            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_LEFT
+                cornerBounces += 1
+
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+            elif log[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
+
+            elif logo[Y]  == WIDTH - 3 and log[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            elif logo[Y] == WIDTH -3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
+
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] - DOWN_RIGHT
+
+            if logo[DIR] != originalDirection:
+                logo[COLOR] = random.choice(COLORS)
+
+
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+
+            elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+
+            elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+
+            elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
+
+    bexts,goto(5,0)
+    bext.fg('white')
+    print('Corner bounces:',cornerBounces,end='')
+
