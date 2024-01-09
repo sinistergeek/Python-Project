deaa3a037e077062136cfc72bc0577d91240f7f4
sinistergeek <susanbhattraidon@gmail.com>
2024-01-09 08:22:01 +0545

    cho_han

---
+++ cho_han.py
@@ -0,0 +1,7 @@
+import random,sys
+JAPANESE_NUMBERs = {1:'ICHI',2:'NI',3:'SAN',4:'SHI',5:'GO',6:'ROKU'}
+print('''
+Cho-Han, by sinistergeek
+      In this traditional Japanese dice Game, two dice are rolled in a bamboo cup byu the dealer sitting on the floor. The player must guess if the dice total to an even (cho) or odd(han) number.
+      ''')
+purse = 5000
