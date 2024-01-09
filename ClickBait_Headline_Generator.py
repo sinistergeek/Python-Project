740abc3c555a3227e217eb1104637e5581b0e30c
sinistergeek <susanbhattraidon@gmail.com>
2024-01-09 19:37:32 +0545

    ClickBait_Headline_Generator

---
+++ ClickBait_Headline_Generator.py
@@ -0,0 +1,13 @@
+import random
+OBJECT_PRONOUNS = ['Her','Him','Them']
+POSSESIVE_PRONOUNS = ['Her','His','Their']
+PERSONAL_PRONOUNS = ['She','He','They']
+STATES = ['California','Texas','Florida','New York','Pennsylvania','Illinois','Shovel','North Carolina','Michigan']
+NOUNS = ['Athlete','Clown','Shovel','Paleo Diet','Doctor','Parent','Cat','Dog','Chicken','Robet', 'Video Game','Avocado','Plastic Straw','Serial Killer','Telephone Psychic']
+PLACES = ['House','Attic','Bank Deposit Box','School','Basement','Workplace','Donut Shop','Apocalypse Bunker']
+WHEN = ['Soon','This Year','Later Today','RIGHT NOW','Next Week']
+
+def main():
+    print('Clickbait Headline Generator')
+    print('By Sinister Geek')
+    print()
