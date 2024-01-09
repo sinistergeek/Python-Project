a4e27430db55edaa54e373207a4b8a28ed7bd557
sinistergeek <susanbhattraidon@gmail.com>
2024-01-09 13:49:02 +0545

    cho_han

--- cho_han.py
+++ cho_han.py
@@ -1,5 +1,5 @@
 import random,sys
-JAPANESE_NUMBERs = {1:'ICHI',2:'NI',3:'SAN',4:'SHI',5:'GO',6:'ROKU'}
+JAPANESE_NUMBERS = {1:'ICHI',2:'NI',3:'SAN',4:'SHI',5:'GO',6:'ROKU'}
 print('''
 Cho-Han, by sinistergeek
       In this traditional Japanese dice Game, two dice are rolled in a bamboo cup byu the dealer sitting on the floor. The player must guess if the dice total to an even (cho) or odd(han) number.
@@ -10,7 +10,7 @@ while True:
     while True:
         pot = input('> ')
         if pot.upper() == 'QUIT':
-            print(Thanks for playing!)
+            print('Thanks for playing!')
             sys.exit()
 
         elif not pot.isdecimal():
@@ -28,8 +28,8 @@ while True:
     print('dice and asks for your bet.')
     print()
     print('     CHO (even) or HAN (ODD)?')
-    while:
-        bet = int('>    ').upper()
+    while True:
+        bet = input('> ').upper()
         if bet != 'CHO' and bet != 'HAN':
             print('Please enter either "CHO" or "HAN".')
             continue
@@ -37,7 +37,8 @@ while True:
             break
     print('The dealer lifts the cup to reveal:')
     print(' ',JAPANESE_NUMBERS[dice1],'-',JAPANESE_NUMBERS[dice2])
-    print('     ',dic1,'-',dice2)
+    print('     ',dice1,'-',dice2)
+    rollIsEven = (dice1 + dice2) %2 == 0
     if rollIsEven:
         correctBet = 'CHO'
     else:
@@ -52,7 +53,7 @@ while True:
         purse = purse - pot
         print('You lost!')
 
-    if purse == 0
-    print("you have has run out of money!")
-    print('Thanks for playing!')
-    sys.exiit()
+    if purse == 0:
+        print("you have has run out of money!")
+        print('Thanks for playing!')
+        sys.exiit()
