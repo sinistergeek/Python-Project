86756e8986b1559be10cac68f48d439aaec1cfd9
sinistergeek <susanbhattraidon@gmail.com>
2024-01-10 15:59:36 +0545

    Collatz_Sequence

---
+++ Collatz_Sequence.py
@@ -0,0 +1,8 @@
+import sys, time
+print('''
+    Collatz Sequence, or, the 3n + 1 Problem by sinistergeek
+    The Collatz sequence is a sequence of numbers produced from a starting number  n, following three rules:
+    1) If n is even, the next number n is n / 2.
+    2) If n is odd, next number n is n *3 + 1.
+      ''')
+
