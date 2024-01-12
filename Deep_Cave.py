d92e69ad235d3b2743c6f78cc7969a540d4ae2e2
sinistergeek <susanbhattraidon@gmail.com>
2024-01-12 15:57:42 +0545

    Deep_Cave

--- Deep_Cave.py
+++ Deep_Cave.py
@@ -9,3 +9,17 @@ gapWidth = 10
 while True:
     rightWidth = WIDTH - gapWidth - leftWidth
     print(('#'*leftWidth) + (' '* gapWidth) + ('#'*rightWidth))
+    try:
+        time.sleep(PAUSE_AMOUNT)
+    except KeyBoardInterrupt:
+        sys.exit()
+    diceRolll = random.randint(1,6)
+    if diceRoll == 1 and leftWidth > 1:
+        leftWidth == 1 and leftWidth - 1
+    elif diceRoll == 2 and leftWidth + gapWidth < WIDTH - 1:
+        leftWidth = leftWidth + 1
+    else:
+        pass
+
+            
+
