fc2a40c96f2f7f93a8d8f9f3f9346107af9e620f
sinistergeek <susanbhattraidon@gmail.com>
2024-01-13 09:45:27 +0545

    Dice_Math

--- Dice_Math.py
+++ Dice_Math.py
@@ -56,3 +56,40 @@ D5 = ([
     '|   O  |',
     '|O    O|'
     ],5)
+
+D6a = ([
+    '+------+',
+    '|O    O|',
+    '|O    O|',
+    '|O    O|'
+    ],6)
+
+D6b = ([
+    '+------+',
+    '|O O O |',
+    '|      |',
+    '|O O O |'],6)
+
+ALL_DICE = [D1,D2a,D2b,D3a,D3b,D4,D5,D6a,D6b]
+print('''
+      Dice Math, by sinstergeek
+      Add up  the sides of all the dice displayed on the screen. You have {} seconds to answer as many as possible. You get {} points for each correct answer and lose {} point for each incorrect answer.
+'''.format(QUIZ_DURATION,REWARD,PLENTY))
+input('Press Enter to begin....')
+
+correctAnswers = 0
+incorrectAnswers = 0
+startTime = time.time()
+while time.time() < startTime + QUIZ_DURATION:
+    sumAnswer = 0
+    diceFaces = []
+    for i in range(random.randint(MID_DICE,MAX_DICE)):
+        die = random.choice(ALL_DICE)
+        diceFaces.append(die[0])
+        sumAnswer += die[1]
+    topLeftDiceCorners = []
+    for i in range(len(diceFaces)):
+        while True:
+            left = random.randint(0,CANVAS_WIDTH - 1 - DICE_WIDTH)
+            top = random.randint(0,CANVAS_HEIGHT - 1 - DICE_HEIGHT)
+
