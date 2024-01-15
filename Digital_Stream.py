6498e0cdd61248a1755c3ee89f98c0fa8184239a
sinistergeek <susanbhattraidon@gmail.com>
2024-01-15 16:33:53 +0545

    Digital_Stream

--- Digital_Stream.py
+++ Digital_Stream.py
@@ -5,4 +5,25 @@ PAUSE = 0.1
 STREAM_CHARS = ['0','1']
 DENSITY = 0.02
 WIDTH = shutil.get_terminal_size()[0]
+WIDTH -= 1
+print('Digital Stream, by sinster-geek')
+print('Press Ctrl-C to quit.')
+time.sleep(2)
+try:
+    columns = [0] * WIDTH
+    while True:
+        for i in range(WIDTH):
+            if columns[i] == 0:
+                if random.random() <= DENSITY:
+                    columns[i] = random.randint(MIN_STREAM_LENGTH,MAX_STREAM_LENGTH)
+            if columns[i] > 0:
+                print(random.choice(STREAM_CHARS),end='')
+                columns[i] -= 1
+            else:
+                print(' ',end='')
+        print()
+        sys.stdout.flush()
+        time.sleep(PAUSE)
 
+except KeyboardInterrupt:
+    sys.exit()
