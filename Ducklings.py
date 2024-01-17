19a7a686790d466b5efec161345c273920b106b4
sinistergeek <susanbhattraidon@gmail.com>
2024-01-17 13:35:08 +0545

    Ducklings

---
+++ Ducklings.py
@@ -0,0 +1,14 @@
+import random, shutil, sys, time
+PAUSE = 0.2
+DENSITY = 0.10
+DUCKLING_WIDTH=5
+LEFT = 'left'
+RIGHT = 'right'
+BEADY = 'beady'
+WIDE = 'wide'
+HAPPY = 'happy'
+ALOOF = 'aloof'
+CHUBBY = 'chubby'
+VERY_CHUBBY = 'very chubby'
+OPEN = 'open'
+CLOSED = 'closed'
