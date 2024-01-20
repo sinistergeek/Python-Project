6bb256dc80ea715c25ad443c24bea92a88ea0bf0
sinistergeek <susanbhattraidon@gmail.com>
2024-01-20 12:05:01 +0545

    Fast_Draw

--- Fast_Draw.py
+++ Fast_Draw.py
@@ -7,3 +7,25 @@ print('When you see "DRAW",you have 0.3 seconds to press Enter.')
 print('But you lose if you press Enter before "DRAW" appears.')
 print()
 input('Press Enter to begin...')
+while True:
+    print()
+    print('It is high noon....')
+    time.sleep(random.randint(20,50)/10.0)
+    print('DRAW!')
+    drawTime = time.time()
+    input()
+    timeElapsed = time.time() - drawTime
+    if timeElapsed < 0.01:
+        print('You drew before "DRAW" appeared! You lose.')
+    elif timeElapsed > 0.3:
+        timeElapsed = round(timeElapsed, 4)
+        print('You took',timeElapsed, 'seconds to draw. Too slow!')
+    else:
+        timeElapsed = round(timeElapsed,4)
+        print('You took',timeElapsed, 'seconds to draw.')
+        print('You are the fastest draw in the west! You win!')
+    print('Enter QUIT to stop, or press Enter to play again.')
+    response = input('> ').upper()
+    if response == 'QUIT':
+        print('Thanks for playing!')
+        sys.exit()
