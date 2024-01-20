a1dc4565c3c57012a8b206ea617045c7643c1f5a
sinistergeek <susanbhattraidon@gmail.com>
2024-01-20 11:56:04 +0545

    Fast_Draw

---
+++ Fast_Draw.py
@@ -0,0 +1,9 @@
+import random,sys,time
+print('Fast Draw, by sinistergeek')
+print()
+print('Time to test your reflexes and see if you are the fastest')
+print('draw in the west!')
+print('When you see "DRAW",you have 0.3 seconds to press Enter.')
+print('But you lose if you press Enter before "DRAW" appears.')
+print()
+input('Press Enter to begin...')
