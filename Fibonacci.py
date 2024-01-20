35ba7727c16133628a4e61abc14ca94cf6066265
sinistergeek <susanbhattraidon@gmail.com>
2024-01-20 17:17:57 +0545

    Fibonacci

--- Fibonacci.py
+++ Fibonacci.py
@@ -13,3 +13,40 @@ while True:
 
         if response.isdecimal() and int(response) != 0:
             nth = int(response)
+            break
+        print('Please enter a number greater than 0, or QUIT.')
+    print()
+    if nth == 1:
+        print('0')
+        print()
+        print('The #1 Fibonacci number is 0.')
+        continue
+    elif nth==2:
+        print('0,1')
+        print()
+        print('The #2 Fibbonacci number is 1.')
+        continue
+    if nth >= 10000:
+        print('WARNING: THis will take a while to display on the ')
+        print('screen. If you want to quit this program before it is')
+        print('done,press Ctrl-C.')
+        input('Press Enter to begin....')
+
+    secondToLastNumber = 0
+    lastNumber = 1
+    fibNumbersCalculated =2
+    print('0,1,',end='')
+
+    while True:
+        nextNumber = secondToLastNumber + lastNumber
+        fibNumbersCalculated += 1
+        print(nextNumber, end='')
+        if fibNumberrsCalculated == nth:
+            print()
+            print()
+            print('The #',fibNumbersCalculated,'Fibonacci','number is ',nextNumber, sep='')
+            break
+        print(',',end='')
+        secondToLastNumber = lastNumber
+        lastNumber = nextNumber
+
