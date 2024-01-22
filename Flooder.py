1a3038faad1f5b85fc0607ed24dc58ec5f885314
sinistergeek <susanbhattraidon@gmail.com>
2024-01-22 18:52:18 +0545

    Flooder

--- Flooder.py
+++ Flooder.py
@@ -64,3 +64,92 @@ def getNewBoard():
         x = random.randint(0, BOARD_WIDTH - 2)
         y = random.randint(0,BOARD_HEIGHT - 1)
     return board
+
+def displayBoard(board,displayMode):
+    bext.fg('white')
+    print(DOWNRIGHT + (LEFTRIGHT * BOARD_WIDTH) + DOWNLEFT)
+    for y in range(BOARD_HEIGHT):
+        bext.fg('white')
+        if y == 0:
+            print('> ',end='')
+        else:
+            print(UPDOWN, end='')
+        for x in range(BOARD_WIDTH):
+            bext.fg(COLORS_MAP[board[(x,y)]])
+            if displayMode == COLOR_MODE:
+                print(BLOCK,end=' ')
+            elif displayMode == SHAPE_MODE:
+                print(SHAPES_MAP[board[(x,y)]],end='')
+        bext.fg('white')
+        print(UPDOWN)
+    print(UPRIGHT + (LEFTRIGHT * BOARD_WIDTH) + UPLEFT)
+
+def askForPlayerMove(displayMode):
+    while True:
+        bext.fg('white')
+        print('Choose one of',end='')
+        if displayMode == COLOR_MODE:
+            bext.fg('red')
+            print('(R)ed',end='')
+            bext.fg('green')
+            print('(G)reen',end='')
+            bext.fg('blue')
+            print('(B)lue',end='')
+            bext.fg('yellow')
+            print('(Y)ellow',end='')
+            bext.fg('cyan')
+            print('(C)yan',end='')
+            bext.fg('purple')
+            print('(P)urple',end='')
+
+        elif displayMode == SHAPE_MODE:
+            bext.fg('red')
+            print('(H)eart',end='')
+            bext.fg('green')
+            print('(T)riange',end='')
+            bext.fg('blue')
+            print('(D)iamond',end='')
+            bext.fg('yellow')
+            print('(B)all',end='')
+            bext.fg('cyan')
+            print('(C)lub',end='')
+            bext.fg('purple')
+            print('(S)pade',end='')
+        bext.fg('white')
+        print('or QUIT')
+        response = input('> ').upper()
+        if response == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        if displayMode == COLOR_MODE and response in tuple('RGBYCP'):
+            return {'R':0,'G':1,'B':2,'Y':3,'C':4,'P':5}[response]
+        if displayMode == SHAPE_MODE and response in tuple('HTDBCS'):
+            return {'H':0,'T':1,'D':2,'B':3,'C':4,'S':5}[response]
+
+def changeTile(titleType,board,x,y,charToChange=None):
+    if x == 0 and y == 0:
+        charToChange =board[(x,y)]
+        if titleType == charToChange:
+            return
+    board[(x,y)] = titleType
+
+    if x > 0 and board[(x - 1,y)] == charToChange:
+        changeTile(tileType,board,x - 1,y,charToChange)
+    if y > 0 and board[(x,y - 1)] == charToChange:
+        changeTile(tileType, board,x,y-1,charToChange)
+    if x < BOARD_WIDTH - 1 and board[(x+1,y)] == charToChange:
+        changeTitle(titleType,board,x+1,y,charToChange)
+    if y < BOARD_HEIGHT - 1 and board[(x,y+1)] == charToChange:
+        changeTile(tileType,board,x,y+1,charToChange)
+
+def hasWon(board):
+    tile = board[(0,0)]
+    for x in range(BOARD_WIDTH):
+        for y in range(BOARD_HEIGHT):
+            if board[(x,y)] != tile:
+                return False
+
+    return True
+
+if __name__ == '__main__':
+    main()
