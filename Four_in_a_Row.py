baccf9abd991c739a18768d12cfd48937b23754e
sinistergeek <susanbhattraidon@gmail.com>
2024-01-24 17:25:58 +0545

    Four_in_a_Row

--- Four_in_a_Row.py
+++ Four_in_a_Row.py
@@ -61,3 +61,43 @@ def askForPlayerMove(playerTile,board):
             continue
         columnIndex =int(response)
         if board[(columnIndex,0)] != EMPTY_SPACE:
+            print('That column is full, select another one.')
+            continue
+        for rowIndex in range(BOARD_HEIGHT -1,-1,-1):
+            if board[(columnIndex,rowIndex)] == EMPTY_SPACE:
+                return (columnIndex,rowIndex)
+
+def isFull(board):
+    for rowIndex in range(BOARD_HEIGHT):
+        for columnIndex in range(BOARD_WIDTH):
+            if board[(columnIndex,rowIndex)] == EMPTY_SPACE:
+                return False
+    return True
+
+
+def isWinner(playerTile,board):
+    for columnIndex in range(BOARD_WIDTH  -3):
+        for rowIndex in range(BOARD_HEIGHT):
+            tile1 =board[(columnIndex,rowIndex)]
+            tile2 =board[(columnIndex +1,rowIndex)]
+            tile3 =board[(columnIndex +2,rowIndex)]
+            tile4 =board[(columnIndex +3,rowIndex)]
+            if tile1 == tile2 == tile3 == tile4 == playerTile:
+                return True
+    for columnIndex in range(BOARD_WIDTH):
+        for rowIndex in range(BOARD_HEIGHT - 3):
+            tile1 = board[(columnIndex, rowIndex)]
+            tile2 = board[(columnIndex, rowIndex + 1)]
+            tile3 = board[(columnIndex, rowIndex + 2)]
+            tile4 = board[(columnIndex, rowIndex + 3)]
+            if tile1 == tile2 == tile3 == tile4 == playerTile:
+                return True
+
+    for columnIndex in range(BOARD_WIDTH - 3):
+        for rowIndex in range(BOARD_HEIGHT - 3):
+            tile1 = board[(columnIndex,rowIndex)]
+            tile2 = board[(columnIndex +1,rowIndex + 1)]
+            tile3 = board[(columnIndex +2,rowIndex + 2)]
+            tile4 = board[(columnIndex +3,rowIndex + 3)]
+            if tile1 == tile2 == tile3 == tile4 == playerTile:
+                return True
