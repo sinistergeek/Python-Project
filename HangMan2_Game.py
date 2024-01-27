9e99c90fb114f8c096ab0125dc161bcdcf7da3be
sinistergeek <susanbhattraidon@gmail.com>
2024-01-27 16:27:52 +0545

    HangMan2_Game

--- HangMan2_Game.py
+++ HangMan2_Game.py
@@ -56,4 +56,16 @@ r"""
     |
                 """
                 ]
+CATEGORY = 'Animals'
+WORDS='ANT BABOON BADER BAT BEAR BEAVER CAMEL CAT CLAM COBRA COUGAR COYOTE CROW DEER DOG DONKEY DUCK EAGLE FERRET FOX FROG GOAT GOOSE HAWK LION LIZARD LLAMA MOLE MONKEY MOOSE MOUSE MULE NEWT OTTER OWL PANDA PARROT PIGEON PYTHON RABBIT RAM RAT RAVEN RHINO SALMON SEAL SHARK SHEEP SKUNK SLOTH SNAKE SPIDER STROK SWAN TIGER TOAD TROUT TURKEY TURTLE WEASEL WHALE WOLF WOMBAT ZEBRA'.split()
 
+def main():
+    print('Hangman, by sinistergeek')
+    missedLetters = []
+    correctLetters = []
+    secretWord = random.choice(WORDS)
+    while True:
+        drawHangman(missedLetters, correctLetters, secretWord)
+        guess = getPlayerGuess(missedLetters + correctLetters)
+        if guess in secretWord:
+            foundAllLetters = True
