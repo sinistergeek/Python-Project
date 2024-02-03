f04748c9bbc31fac56f6ddf370a8d9f6d13e5393
sinistergeek <susanbhattraidon@gmail.com>
2024-02-03 16:55:34 +0545

    Leetspeak

--- Leetspeak.py
+++ Leetspeak.py
@@ -16,4 +16,25 @@ def main():
     try:
         pyperclip.copy(leetspeak)
         print('(Copied leetspeak to clipboard.)')
+    except NameError:
+        pass
 
+def englishToLeetspeak(message):
+    charMapping = {
+    'a': ['4', '@', '/-\\'], 'c': ['('], 'd': ['|)'], 'e': ['3'],
+    'f': ['ph'], 'h': [']-[', '|-|'], 'i': ['1', '!', '|'], 'k': [']<'],
+    'o': ['0'], 's': ['$', '5'], 't': ['7', '+'], 'u': ['|_|'],
+    'v': ['\\/']
+            }
+    leetspeak = ''
+    for char in message:
+        if char.lower() in charMapping and random.random() <= 0.70:
+            possibleLeetReplacements = charMapping[char.lower()]
+            leetReplacement =random.choice(possibleLeetReplacements)
+            leetspeak = leetspeak + leetReplacement
+        else:
+            leetspeak = leetspeak + char
+    return leetspeak
+
+if __name__ == '__main__':
+    main()
