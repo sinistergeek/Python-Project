9ae581f8fb0a56d77452a1d5c013630095fd2e7f
sinistergeek <susanbhattraidon@gmail.com>
2024-02-03 14:57:18 +0545

    Langton_Ant

--- Langton_Ant.py
+++ Langton_Ant.py
@@ -5,4 +5,41 @@ except ImportError:
     print('This program requires the bext module, which you')
     sys.exit()
 
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+HEIGHT -= 1
+NUMBER_OF_ANTS = 10
+PAUSE_AMOUNT = 0.1
+ANT_UP = '^'
+ANT_DOWN = 'v'
+ANT_LEFT = '<'
+ANT_RIGHT = '>'
 
+ANT_COLOR = 'red'
+BLACK_TITLE = 'black'
+WHITE_TILE = 'white'
+
+NORTH = 'north'
+SOUTH = 'south'
+EAST = 'east'
+WEST = 'west'
+
+def main():
+    bext.fg(ANT_COLOR)
+    bext.bg(WHITE_TILE)
+    bext.clear()
+
+    board = {'width':WIDTH,'height':HEIGHT}
+
+    ants = []
+    for i in range(NUMBER_OF_ANTS):
+        ant = {
+                'x': random.randint(0,WIDTH - 1),
+                'y': random.randint(0,HEIGHT - 1),
+                'direction': random.choice([NORTH,SOUTH,EAST,WEST])
+                }
+        ants.append(ant)
+    changedTiles = []
+    while True:
+        displayBoard(board,ants,changedTiles)
+        changedTiles = []
