8f807ade33e727a45c6b825ac413e31b5dea5440
sinistergeek <susanbhattraidon@gmail.com>
2024-02-04 18:09:30 +0545

    Mancala

--- Mancala.py
+++ Mancala.py
@@ -20,4 +20,33 @@ def main():
     playerTurn = '1'
     while True:
         print('\n'* 60)
+        displayBoard(gameBoard)
+        playerMove = askForPlayerMove(playerTurn,gameBoard)
+        playerTurn = makeMove(gameBoard,playerTurn,playerMove)
+        winner = checkForWinner(gameBoard)
+        if winner == '1' or winner == '2':
+            displayBoard(gameBoard)
+            print('Player' + winner + 'has won!')
+            sys.exit()
+        elif winner == 'tie':
+            displayBoard(gameBoard)
+            print('There is a tie!')
+            sys.exit()
+
+def getNewBoard():
+    s = STARTING_NUMBER_OF_SEEDS
+    return {'1':0,'2':0,'A':s,'B':s,'C':s,'D':s,'E':s,'F':s,'G':s,'H':s,'I':s,'J':s,'K':s,'L':s}
+
+def displayBoard(board):
+    seedAmounts = []
+
+    for pit in 'GHIJKL21ABCDEF':
+        numSeedsInThisPit = str(board[pit]).rjust(2)
+        seedAmounts.append(numSeedsInThisPit)
+        print("""
+        +---------+---------+--------+-----+---------+
+        G{} | H{} |I{} | J{}| K{} | L{}
+        A{} | B{} |C{} | D{}| E{} | F{}
+        +---------+---------+--------+-----+---------+
+        """.format(*seedAmounts))
 
