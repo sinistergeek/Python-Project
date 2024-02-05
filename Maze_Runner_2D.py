be84b0bb9ed57696cd9386e6292793c9583cc81e
sinistergeek <susanbhattraidon@gmail.com>
2024-02-05 22:34:05 +0545

    Maze_Runner_2D

--- Maze_Runner_2D.py
+++ Maze_Runner_2D.py
@@ -27,4 +27,52 @@ MAze Runner 2D
 while True:
     print('Enter the filename of the maze(or list or Quit)')
     filename = input('> ')
+    if filename.upper() == 'LIST':
+        print('Maze files found in', os.getcwd())
+        for fileInCurrentFolder in os.getcwd():
+            if(fileInCurrentFolder.startswith('maze') and fileInCurrentFolder.endswith('.txt')):
+                print(' ',fileInCurrentFolder)
+        continue
+    if filename.upper() == 'QUIT':
+        sys.exit()
 
+    if os.path.exists(filename):
+        break
+    print('There is no file name',filename)
+
+mazefile = open(filename)
+maze = {}
+lines = mazeFile.readlines()
+playerx = None
+playery = None
+exitx = None
+exity = None
+y = 0
+
+for line in lines:
+    WIDTH = len(line.rstrip())
+    for x,character in enumerate(linerstrip()):
+        assert character in (WALL,EMPTY,START,EXIT),'Invalid character at column {},line {}'.format(x + 1, y + 1)
+        if character in (WALL, EMPTY):
+            maze[(x,y)] = character
+        elif character == START:
+            playerx,playery = x,y
+            maze[(x,y)] = EMPTY
+        elif character == EXIT:
+            exitx,exity = x,y
+            maze[(x,y)] = EMPTY
+    y += 1
+HEIGHT = y
+
+assert playerx != None and playery != None,'No start in maze file.'
+assert exitx != None and exity != None, 'No exit in maze file.'
+
+While True:
+    displayMaze(maze)
+    while True:
+        print('         W')
+        print('Enter direction, or QUIT: ASD')
+        move = input('> ').upper()
+        if move == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
