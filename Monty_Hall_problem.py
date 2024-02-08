217f028ebb437bf387fc9170e443ffa3992849f2
sinistergeek <susanbhattraidon@gmail.com>
2024-02-08 08:56:15 +0545

    Monty_Hall_problem

---
+++ Monty_Hall_problem.py
@@ -0,0 +1,35 @@
+import random,sys
+
+ALL_CLOSED = """
++  + +  + +  +
+ 1    2    3
+"""
+
+FIRST_GOAT = """
++  + +  + +  +
+ go    2    3
+"""
+
+SECOND_GOAT = """
++  + +  + +  +
+  1   go    3
+"""
+
+THIRD_GOAT = """
++  + +  + +  +
+  1    2   go
+"""
+
+FIRST_CAR_OTHERS_GOAT = """
++  + +  + +  +
+ car  go   go
+"""
+
+SECOND_CAR_OTHERS_GOAT = """
++  + +  + +  +
+ go   car   go
+"""
+THIRD_CAR_OTHERS_GOAT = """
++  + +  + +  +
+go    go   car
+"""
