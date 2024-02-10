ab05c1e13381520bf3bea66fc651b5c2b2803e6f
sinistergeek <susanbhattraidon@gmail.com>
2024-02-10 21:03:34 +0545

    Prime_Numbers

--- Prime_Numbers.py
+++ Prime_Numbers.py
@@ -9,3 +9,31 @@ def main():
     while True:
         print('Enter a number to start searching for primes from:')
         print('(Try 0 or 100000000000 (n zero) or another number.')
+        response = input('> ')
+        if response.isdecimal():
+            num = int(response)
+            break
+
+    input('Press Ctrl-C at any time to quit. Press Enter to begin.....')
+    while True:
+        if isPrime(num):
+            print(str(num) + ', ', end='',flush=True)
+        num = num + 1
+
+def isPrime(number):
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+
+    for i in range(2,int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
