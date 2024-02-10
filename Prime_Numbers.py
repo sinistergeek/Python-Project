76744abfad822c47cbfe0a7fa517ebd4d8e04e9f
sinistergeek <susanbhattraidon@gmail.com>
2024-02-10 20:57:51 +0545

    Prime_Numbers

---
+++ Prime_Numbers.py
@@ -0,0 +1,11 @@
+import math, sys
+
+def main():
+    print('''
+    Prime Numbers 
+    It is the numbers that are only evenly divisible by one and themselves. They are used in a variety of practical applications but can't be predicted. They must be calculated one at a time.''')
+    print()
+
+    while True:
+        print('Enter a number to start searching for primes from:')
+        print('(Try 0 or 100000000000 (n zero) or another number.')
