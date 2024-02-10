e4307f83a816d3fdda060e16b49dd50de1a7be61
sinistergeek <susanbhattraidon@gmail.com>
2024-02-10 18:22:03 +0545

    periodictable

--- periodictable.py
+++ periodictable.py
@@ -36,3 +36,41 @@ for line in elements:
 
 print('Periodic Table of Elements')
 print()
+
+while True:
+    print('''
+    Periodic Table of Elements
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+
+1 H
+He
+
+2 Li Be
+B C N O F Ne
+
+3 Na Mg
+Al Si P S Cl Ar
+
+4 K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+
+5 Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe
+
+6 Cs Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
+
+7 Fr Ra Ac Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
+
+Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu
+
+Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr
+    ''')
+
+    print('Enter a symbol or atomic number to examine, or QUIT to quit.')
+    response = input('> ').title()
+    if response == 'Quit':
+        sys.exit()
+
+    if response in ELEMENTS:
+        for key in ALL_COLUMNS:
+            keyJustified = key.rjust(LONGEsT_COLUMN)
+            print(keyJustified + ': '+ ELEMENTS[response][key])
+        input('Press Enter to continue...')
