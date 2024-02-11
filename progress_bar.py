9d2c1ad04311f31636fcfeb6205dafb53fe5e73f
sinistergeek <susanbhattraidon@gmail.com>
2024-02-11 16:10:34 +0545

    progress_bar

--- progress_bar.py
+++ progress_bar.py
@@ -9,3 +9,27 @@ def main():
         bytesDownload += random.randint(0,100)
         barStr = getProgressBar(bytesDownloaded,downloadSize)
         print(barStr,end='',flush = True)
+        time.sleep(0.2)
+        print('\b' * len(barStr), end ='',flush=True)
+
+def getProgressBar(progress,total,barWidth=40):
+    progressBar = ''
+    progressBar += '['
+
+    if progress > total:
+        progress = total
+    if progress < 0:
+        progress = 0
+
+    numberOfBars = int((progress / total) * barWidth)
+    progressBar += BAR * numberOfBars
+    progressBar += ' ' * (barWidth - numberOfBars)
+    progressBar += ']'
+
+    percentComplete =round(progress / total * 100,1)
+    progressBar += ' ' + str(percentComplete) + '%'
+    progressBar += ' ' + str(progress) + '/' + str(total)
+    return ProgressBar
+
+if __name__ == '__main__':
+    main()
