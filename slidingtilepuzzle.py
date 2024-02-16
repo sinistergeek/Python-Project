2d2d94c8dfbad7051618e9e1f8c00fe22f3ee08d
sinistergeek <susanbhattraidon@gmail.com>
2024-02-16 15:39:12 +0545

    slidingtilepuzzle

--- slidingtilepuzzle.py
+++ slidingtilepuzzle.py
@@ -51,3 +51,43 @@ def askForPlayerMove(board):
         print('Enter WASD (or QUIT): ({}) ({}) ({})'.format(a,s,d))
         response = input('> ').upper()
         if response == 'QUIT':
+            sys.exit()
+        if response in (w + a + s + d).replace(' ',''):
+            return response
+
+
+def makeMove(board,move):
+    bx, by = findBlankSpace(board)
+    if move == 'W':
+        board[bx][by],board[bx][by+1] = board[bx][by+1],board[bx][by]
+
+    elif move == 'A':
+        board[bx][by],board[bx+1][by] = board[bx+1][by],board[bx][by]
+
+    elif move == 'S':
+        board[bx][by],board[bx][by-1] = board[bx][by-1 ],board[bx][by]
+    elif move == 'D':
+        board[bx][by],board[bx-1][by] = board[bx-1][by],board[bx][by]
+
+
+def makeRandomMove(board):
+    blankx, blanky = findBlankSpace(board)
+    validMoves = []
+    if blanky != 3:
+        validMoves.append('W')
+    if blankx != 3:
+        validMoves.append('A')
+    if blanky != 0:
+        validMoves.append('S')
+    if blankx != 0:
+        validMoves.append('D')
+    makeMove(board,random.choice(validMoves))
+
+def getNewPuzzle(moves=200):
+    board = getNewBoard()
+    for i in range(moves):
+        makeRandomMove(board)
+    return board
+
+if __name__ == '__main__':
+    main()
