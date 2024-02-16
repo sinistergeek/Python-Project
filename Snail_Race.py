827aec1182b0a306b6024de0dcfd3c8d9459bdff
sinistergeek <susanbhattraidon@gmail.com>
2024-02-16 18:37:11 +0545

    Snail_Race

--- Snail_Race.py
+++ Snail_Race.py
@@ -7,3 +7,45 @@ print(''' Snail Race
       @v <-- snail
 ''')
 
+while True:
+    print('How many snails will race Max:',MAX_NUM_SNAIL)
+    response = input('> ')
+    if response.isdecimal():
+        numSnailsRacing = int(response)
+        if 1 < numSnailRacing <= MAX_NUM_SNAILS:
+            break
+    print('Enter a number between 2 and',MAX_NUM_SNAILS)
+
+snailNames = []
+for i in range(1, numSnailsRacing + 1):
+    while True:
+        print('Enter snail #' + str(i) + "'s name:" )
+        name = input('> ')
+        if len(name) == 0:
+            print('Please enter a name.')
+        elif name in snailNames:
+            print('Choose a name that has not already been used.')
+        else:
+            break
+    snailNames.append(name)
+
+print('\n' * 40)
+print('START' + (' ' * (FINISH_LINE - len('START')) + 'FINISH'))
+print('|' + (' ' * (FINISH_LINE - len('|')) + '|'))
+snailProgress = {}
+for snailName in snailNames:
+    print(snailName[:MAX_NAME_LENGTH])
+    print('@v')
+    snailProgress[snailName] = 0
+
+time.sleep(1.5)
+
+while True:
+    for i in range(random.randint(1,numSnailsRacing // 2)):
+        randomSnailName = random.choice(snailNames)
+        snailProgress[randomSnailName] += 1
+        if snailProgress[randomSnailName] == FINISH_LINE:
+            print(randomSnailName, 'has won!')
+            sys.exit()
+    time.sleep(0.5)
+    print('\n' * 40)
