d084e15ba29bb676ac65a8b39cf9f6d83aacb985
sinistergeek <susanbhattraidon@gmail.com>
2024-02-16 08:14:41 +0545

    slidingtilepuzzle

--- slidingtilepuzzle.py
+++ slidingtilepuzzle.py
@@ -18,3 +18,36 @@ def main():
         if gameBoard == getNewBoard():
             print('You won!')
             sys.exit()
+
+def getNewBoard():
+    return [['1','5','9','13'],['2','6','10','14'],['3','7','11','15'],['4','8','12',BLANK]]
+
+def displayBoard(board):
+    labels = [board[0][0],board[1][0],board[2][0],board[3][0],board[0][1],board[1][1],board[2][1],board[3][1],board[0][2],board[1][2],board[2][2],board[3][2],board[0][3],board[1][3],board[2][3],board[3][3]]
+    boardToDraw = """
+    +---+---+---+---+
+     {}   {}  {}  {}
+     {}   {}  {}  {}
+     {}   {}  {}  {}
+     {}   {}  {}  {}
+    """.format(*labels)
+    print(boardToDraw)
+
+
+def findBlankSpace(board):
+    for x in range(4):
+        for y in range(4):
+            if board[x][y] == ' ':
+                return (x,y)
+
+
+def askForPlayerMove(board):
+    w = 'W'
+    a = 'A'
+    s = 'S'
+    d = 'D'
+    while True:
+        print('     ({})'.format(w))
+        print('Enter WASD (or QUIT): ({}) ({}) ({})'.format(a,s,d))
+        response = input('> ').upper()
+        if response == 'QUIT':
