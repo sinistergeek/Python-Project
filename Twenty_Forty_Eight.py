50373b1163f43fa97deca6075ede3a00d525a43d
sinistergeek <susanbhattraidon@gmail.com>
2024-02-20 12:41:21 +0545

    Twenty_Forty_Eight

--- Twenty_Forty_Eight.py
+++ Twenty_Forty_Eight.py
@@ -72,3 +72,66 @@ def combineTilesInColumn(column):
                 combinedTiles[aboveIndex] = combinedTiles[aboveIndex + 1]
             combinedTiles[3] = BLANK
     return combinedTiles
+
+
+def makeMove(board,move):
+    if move == 'W':
+        allColumnsSpaces = [[(0,0),(0,1),(0,2),(0,3)
+                ],[(1,0),(1,1),(1,2),(1,3)],[(2,0),(2,1),(2,2),(2,3)],[(3,0),(3,1),(3,2),(3,3)]]
+    elif move == 'A':
+        allColumnsSpaces =[[(0,0),(1,0),(2,0),(3,0)],[(0,1),(1,1),(2,1),(3,1)],[(0,2),(1,2),(2,2),(3,2)],[(0,3),(1,3),(2,3),(3,3)
+                ]]
+    elif move == 'S':
+        allColumnsSpaces = [[(0,3),(0,2),(0,1),(0,0)],[(1,3),(1,2),(1,1),(1,1),(1,0)],[(2,3),(2,2),(2,1),(2,0)],[(3,3),(3,2),(3,1),(3,0)]]
+    elif move == 'D':
+        allColumnsSpaces = [[(3,0),(2,0),(1,0),(0,0)],[(3,1),(2,1),(1,1),(0,1)],[(3,2),(2,2),(1,2),(0,2)],[(3,3),(2,3),(1,3),(0,3)]]
+    boardAfterMove = {}
+    for columnSpaces in alllColumnsSpaces:
+        firstTileSpace = columnSpaces[0]
+        secondTileSpace = columnSpaces[1]
+        thirdTileSpace = columnSpaces[2]
+        fourthTileSpace = columnSpaces[3]
+
+        firstTile = board[firstTileSpace]
+        secondTile = board[secondTileSpace]
+        thridTile =board[thirdTileSpace]
+        fourthTile =board[fourthTileSpace]
+        column = [firstTile, secondTile,thirdTile,fourthTile]
+        combinedTilesColumn = combineTlesinColumn(column)
+        boardAfterMove[firstTileSpace] = combinedTilesColumn[0]
+        boardAfterMove[secondTileSpace] = combinedTilesColumn[1]
+        boardAfterMove[thirdTileSpace] = combinedTilesColumn[2]
+        boardAfterMove[fourthTileSpace] = combinedTilesColumn[3]
+    return boardAfterMove
+
+def askForPlayerMove():
+    print('Enter move:(WASD or Q to quit)')
+    while True:
+        move = input('> ').upper()
+        if move == 'Q':
+            print('Thanks for playing!')
+            sys.exit()
+        if move in ('W','A','S','D'):
+            return move
+        else:
+            print('Enter one of "W","A","S","D", or "Q".')
+def addTwoBoard(board):
+    while True:
+        randomSpace = (random.randint(0,3),random.randint(0,3))
+        if board[randomSpace] == BLANK:
+            board[randomSpace] = 2
+            return
+
+def isFull(board):
+    for x in range(4):
+        for y in range(4):
+            if board[(x,y)] == BLANK:
+                return False
+    return True
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
