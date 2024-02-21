78fa92d61a4d289ede8e05d671adf4b89f491f24
sinistergeek <susanbhattraidon@gmail.com>
2024-02-21 08:52:35 +0545

    vigenere

--- vigenere.py
+++ vigenere.py
@@ -32,3 +32,41 @@ def main():
         translated = encryptMessage(myMessage,myKey)
     elif myMode == 'decrypt':
         translated = decryptMessage(myMessage,myKey)
+    print('%sed message:' %(myMode.title()))
+    print(translated)
+    try:
+        pyperclip.copy(translated)
+        print('Full %sed text copied to clipboard.'%(myMode))
+    except:
+        pass
+
+def encryptMessage(message,key):
+    return translateMessage(message,key,'encrypt')
+def decryptMessage(message,key):
+    return translateMessage(message,key,'decrypt')
+
+def translateMessage(message,key,mode):
+    translated =[]
+    keyIndex=0
+    key = key.upper()
+    for symbol in message:
+        num = LETTERS.find(symbol.upper())
+        if num != -1:
+            if mode == 'encrypt':
+                num += LETTERS.find(key[keyIndex])
+            elif mode == 'decrypt':
+                num -= LETTERS.find(key[keyIndex])
+            num %= len(LETTERS)
+            if symbol.isupper():
+                translated.append(LETTERS[num])
+            elif symbol.islower():
+                translated.append(LETTERS[num].lower())
+            keyIndex += 1
+            if keyIndex == len(key):
+                keyIndex = 0
+        else:
+            translated.append(symbol)
+
+    return ''.join(translated)
+if __name__ == '__main__':
+    main()
